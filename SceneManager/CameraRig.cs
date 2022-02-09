67be56092b7636eb45b2ea8da259abdeb28225d0
Justin Helps <justin.r.helps@gmail.com>
2022-02-09 12:12:49 -0600

    Add grid overlay option to CameraRig

--- SceneManager/CameraRig.cs
+++ SceneManager/CameraRig.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
-
-
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class CameraRig : PrimerObject
 {
     // Class that handles camera movement and recording
     // Makes it easy to rotate the camera around a center
 
+    [SerializeField] bool gridOverlay;
+    [SerializeField] int xDivisions = 3;
+    [SerializeField] int yDivisions = 3;
+    [SerializeField] int lineWidth = 5;
+    [SerializeField] Slider sliderPrefab = null;
     internal Camera cam;
     RenderTexture rt = null;
     Texture2D image = null;
@@ -22,6 +29,7 @@ public class CameraRig : PrimerObject
     }
     void OnDrawGizmos() {
         Gizmos.DrawSphere(swivelOrigin, 0.1f);
+        UpdateGridOverlay();
     }
 
     [SerializeField] Vector3 swivelOrigin;
@@ -107,5 +115,89 @@ public class CameraRig : PrimerObject
         RenderTexture.active = null;
         Destroy(rt);
         Destroy(image);
+    }
+    void UpdateGridOverlay() {
+        if (gridOverlay) {
+            Canvas canvas = null;
+            if (GameObject.Find("Canvas") == null) {
+                GameObject canvasGO = new GameObject();
+                canvasGO.name = "Canvas";
+                canvas = canvasGO.AddComponent<Canvas>();
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                canvasGO.AddComponent<CanvasScaler>();
+                canvasGO.AddComponent<GraphicRaycaster>();
+            }
+            else { canvas = GameObject.Find("Canvas").GetComponent<Canvas>(); }
+
+            GridLayoutGroup vGroup = null;
+            if (GameObject.Find("vGridGroup") == null) {
+                GameObject vGroupGO = new GameObject();
+                vGroupGO.transform.parent = canvas.transform;
+                vGroupGO.name = "vGridGroup";
+                vGroup = vGroupGO.AddComponent<GridLayoutGroup>();
+
+                RectTransform vgrt = vGroupGO.GetComponent<RectTransform>();
+                vgrt.anchorMin = Vector2.zero;
+                vgrt.anchorMax = Vector2.one;
+                vgrt.offsetMin = Vector2.zero;
+                vgrt.offsetMax = Vector2.zero;
+
+                vGroup.childAlignment = TextAnchor.MiddleCenter;
+                vGroup.constraint = GridLayoutGroup.Constraint.FixedRowCount;
+                vGroup.constraintCount = 1;
+            }
+            else { vGroup = GameObject.Find("vGridGroup").GetComponent<GridLayoutGroup>(); }
+            vGroup.cellSize = new Vector2(lineWidth, canvas.GetComponent<RectTransform>().sizeDelta.y);
+            vGroup.spacing = new Vector3( (float) (canvas.GetComponent<RectTransform>().sizeDelta.x - lineWidth) / xDivisions - lineWidth, 0);
+
+            List<Image> vDividers = vGroup.GetComponentsInChildren<Image>().ToList();
+            while (vDividers.Count > xDivisions + 1) {
+                DestroyImmediate(vDividers[0].gameObject);
+                vDividers.RemoveAt(0);
+            }
+            for (int i = vDividers.Count; i < xDivisions + 1; i++) {
+                Image image = new GameObject().AddComponent<Image>();
+                image.gameObject.AddComponent<CanvasRenderer>();
+                image.transform.parent = vGroup.transform;
+            }
+
+            GridLayoutGroup hGroup = null;
+            if (GameObject.Find("hGridGroup") == null) {
+                GameObject hGroupGO = new GameObject();
+                hGroupGO.transform.parent = canvas.transform;
+                hGroupGO.name = "hGridGroup";
+                hGroup = hGroupGO.AddComponent<GridLayoutGroup>();
+
+                RectTransform hgrt = hGroupGO.GetComponent<RectTransform>();
+                hgrt.anchorMin = Vector2.zero;
+                hgrt.anchorMax = Vector2.one;
+                hgrt.offsetMin = Vector2.zero;
+                hgrt.offsetMax = Vector2.zero;
+
+                hGroup.childAlignment = TextAnchor.MiddleCenter;
+                hGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+                hGroup.constraintCount = 1;
+            }
+            else { hGroup = GameObject.Find("hGridGroup").GetComponent<GridLayoutGroup>(); }
+            hGroup.cellSize = new Vector2(canvas.GetComponent<RectTransform>().sizeDelta.x, lineWidth);
+            hGroup.spacing = new Vector3( 0, (float) (canvas.GetComponent<RectTransform>().sizeDelta.y - lineWidth) / yDivisions - lineWidth);
+
+            List<Image> hDividers = hGroup.GetComponentsInChildren<Image>().ToList();
+            while (hDividers.Count > yDivisions + 1) {
+                DestroyImmediate(hDividers[0].gameObject);
+                hDividers.RemoveAt(0);
+            }
+            for (int i = hDividers.Count; i < yDivisions + 1; i++) {
+                Image image = new GameObject().AddComponent<Image>();
+                image.gameObject.AddComponent<CanvasRenderer>();
+                image.transform.parent = hGroup.transform;
+            }
+        }
+        if (!gridOverlay) {
+            GameObject vGroup = GameObject.Find("vGridGroup");
+            if (vGroup != null) { DestroyImmediate(vGroup); }
+            GameObject hGroup = GameObject.Find("hGridGroup");
+            if (hGroup != null) { DestroyImmediate(hGroup); }
+        }
     }
 }
