1383fb914e87a4e2d595e89002edc122f0691676
itsjohncs <johnsullivan.pem@gmail.com>
2022-06-20 12:21:13 -0700

    Render SVG from text dynamically.
    This ends up mucking with the SpriteRenderer's values in the editor.
    I've been having trouble figuring out how to prevent that so I want
    to do that in an additional commit.

--- Primer.Latex/Assets/LatexRendererComponent.cs
+++ Primer.Latex/Assets/LatexRendererComponent.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Unity.VectorGraphics;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class LatexRendererComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void GenerateSprite()
     {
-        
-    }
+        var tessOptions = new VectorUtils.TessellationOptions()
+        {
+            StepDistance = 100.0f,
+            MaxCordDeviation = 0.5f,
+            MaxTanAngleDeviation = 0.1f,
+            SamplingStepSize = 0.01f
+        };
+    
+        // Dynamically import the SVG data, and tessellate the resulting vector scene.
+        SVGParser.SceneInfo sceneInfo;
+        try
+        {
+            sceneInfo = SVGParser.ImportSVG(new StringReader(svgText));
+        }
+        catch (System.Exception e)
+        {
+            if (svgText != "")
+            {
+                Debug.LogError($"Invalid SVG, got error: {e.ToString()}");
+            }
 
-    // Update is called once per frame
-    void Update()
+            GetComponent<SpriteRenderer>().sprite = null;
+            return;
+        }
+
+        var geoms = VectorUtils.TessellateScene(sceneInfo.Scene, tessOptions);
+            
+        // Build a sprite with the tessellated geometry.
+        var sprite = VectorUtils.BuildSprite(geoms, 10.0f, VectorUtils.Alignment.Center, Vector2.zero, 128, true);
+        GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+    
+    public string svgText;
+    private string _lastRenderedSvgTextValue;
+
+    public void Update()
     {
-        
+        if (_lastRenderedSvgTextValue != svgText)
+        {
+            GenerateSprite();
+            _lastRenderedSvgTextValue = svgText;
+        }
     }
 }
