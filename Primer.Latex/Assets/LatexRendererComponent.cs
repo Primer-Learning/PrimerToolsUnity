73535f4f14e33616e02e07dc0f92ff37ee9e552b
itsjohncs <johnsullivan.pem@gmail.com>
2022-06-20 13:31:18 -0700

    Generate DVI from latex.
    This is basically a sketch since its quite noisy and doesn't clean up
    after itself but it does work. I'll fill in the details after I've got
    it working.

--- Primer.Latex/Assets/LatexRendererComponent.cs
+++ Primer.Latex/Assets/LatexRendererComponent.cs
@@ -1,13 +1,81 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 using Unity.VectorGraphics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 [ExecuteInEditMode]
 public class LatexRendererComponent : MonoBehaviour
 {
-    private void GenerateSprite()
+    private static DirectoryInfo CreateTempDirectory()
+    {
+        return Directory.CreateDirectory(
+            Path.Combine(Path.GetTempPath(), $"unity-latex-{Guid.NewGuid().ToString()}"));
+    }
+
+    private static async void RecursivelyDeleteDirectory(DirectoryInfo directory)
+    {
+        await Task.Factory.StartNew(() => directory.Delete(true));
+    }
+
+    private async void GenerateDvi()
+    {
+        DirectoryInfo temporaryDirectory = CreateTempDirectory();
+        Debug.Log($"LaTeX build directory: {temporaryDirectory.FullName}");
+
+        var sourcePath = Path.Combine(temporaryDirectory.FullName, "source.tex");
+        await using (var sourceFile = new FileStream(sourcePath, FileMode.CreateNew))
+        {
+            await using (var writer = new StreamWriter(sourceFile))
+            {
+                await writer.WriteAsync(latex);
+            }
+        };
+
+        try
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo("/Library/TeX/texbin/latex")
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    ArgumentList =
+                    {
+                        "-interaction=batchmode",
+                        "-halt-on-error",
+                        $"-output-directory={temporaryDirectory.FullName}",
+                        sourcePath,
+                    }
+                }
+            };
+
+            using (process)
+            {
+                process.Start();
+                
+                process.OutputDataReceived += (sender, args) => UnityEngine.Debug.Log($"stdout: {args.Data}");
+                process.ErrorDataReceived += (sender, args) => UnityEngine.Debug.Log($"stderr: {args.Data}");
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                
+                // TODO: asyncify
+                process.WaitForExit();
+            }
+        }
+        finally
+        {
+            //RecursivelyDeleteDirectory(temporaryDirectory);
+        }
+    }
+    
+    private void GenerateSprite(string svgText)
     {
         var tessOptions = new VectorUtils.TessellationOptions()
         {
@@ -41,15 +109,15 @@ public class LatexRendererComponent : MonoBehaviour
         GetComponent<SpriteRenderer>().sprite = sprite;
     }
     
-    public string svgText;
-    private string _lastRenderedSvgTextValue;
+    public string latex;
+    private string _lastRenderedLatex;
 
-    public void Update()
+    public async void Update()
     {
-        if (_lastRenderedSvgTextValue != svgText)
+        if (latex != _lastRenderedLatex)
         {
-            GenerateSprite();
-            _lastRenderedSvgTextValue = svgText;
+            GenerateDvi();
+            _lastRenderedLatex = latex;
         }
     }
 }
