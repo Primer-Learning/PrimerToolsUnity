3d34af522223b6595bd6f104ed046a54c0e26fde
itsjohncs <johnsullivan.pem@gmail.com>
2022-06-26 15:32:53 -0700

    Remove some early prototyping files.

--- Primer.Latex/render-latex-to-svg.sh
+++
@@ -1,9 +0,0 @@
-SCRIPT_DIR="$(
-    cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
-    pwd -P
-)"
-
-set -eux
-
-latex -interaction=batchmode -halt-on-error -output-directory=/tmp/ "$1"
-dvisvgm "/tmp/${1%.*}.dvi" --no-fonts --verbosity=0 --output="${1%.*}.svg"
