88326c41042629ac15c18538916343dcf53737d1
itsjohncs <johnsullivan.pem@gmail.com>
2022-07-19 17:10:12 -0700

    WIP: Make it possible to define multiple morphs.

--- Primer.Latex/Editor/Timeline/LatexTransitionClipEditor.cs
+++ Primer.Latex/Editor/Timeline/LatexTransitionClipEditor.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using LatexRenderer.Timeline;
+using UnityEditor.Timeline;
+using UnityEngine;
 
 namespace UnityEditor.LatexRenderer.Timeline
 {
@@ -7,21 +11,59 @@ namespace UnityEditor.LatexRenderer.Timeline
     {
         private LatexTransitionClip Clip => (LatexTransitionClip)target;
 
+        private ExposedReference<Transform> CreateReference(Transform transform)
+        {
+            ExposedReference<Transform> result = new();
+            result.Set(TimelineEditor.inspectedDirector, transform);
+            return result;
+        }
+
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
+            serializedObject.Update();
+            DrawPropertiesExcluding(serializedObject, "morphTransitions");
+            serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.LabelField("Morph Transitions");
+
+            var toDelete = new List<int>();
+
+            var playableDirector = TimelineEditor.inspectedDirector;
+            var morphs = Clip.morphTransitions;
+            for (var i = 0; i < morphs.Count; ++i)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel("Before Child");
+                var oldBeforeChild = morphs[i].beforeChild.Resolve(playableDirector);
+                var newBeforeChild =
+                    EditorGUILayout.ObjectField(oldBeforeChild, typeof(Transform), true) as
+                        Transform;
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel("After Child");
+                var oldAfterChild = morphs[i].afterChild.Resolve(playableDirector);
+                var newAfterChild =
+                    EditorGUILayout.ObjectField(oldAfterChild, typeof(Transform),
+                        true) as Transform;
+                EditorGUILayout.EndHorizontal();
+
+                if (oldBeforeChild != newBeforeChild || oldAfterChild != newAfterChild)
+                    morphs[i] = new LatexTransitionClip.MorphTransition
+                    {
+                        beforeChild = CreateReference(newBeforeChild),
+                        afterChild = CreateReference(newAfterChild)
+                    };
+
+                if (GUILayout.Button("Delete")) toDelete.Add(i);
+            }
+
+            foreach (var i in toDelete.AsEnumerable().Reverse()) morphs.RemoveAt(i);
+
+            if (GUILayout.Button("Add New")) morphs.Add(new LatexTransitionClip.MorphTransition());
+
+            serializedObject.Update();
+
             // serializedObject.Update();
-            // var playableDirector = TimelineEditor.inspectedDirector;
-            // var targets = Clip.targets;
-            // for (var i = 0; i < targets.Count; ++i)
-            // {
-            //     var newValue = EditorGUILayout.ObjectField(targets[i].Resolve(playableDirector),
-            //         typeof(Transform), true) as Transform;
-            //
-            //     ExposedReference<Transform> foo = new();
-            //     foo.Set(playableDirector, newValue);
-            //     targets[i] = foo;
-            // }
             //
             // // var newValue =
             // //     (Transform)EditorGUILayout.ObjectField(Clip.single.Resolve(playableDirector),
