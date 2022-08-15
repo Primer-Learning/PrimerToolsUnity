63b0608f36e0b74387d7b925411799a60f360fca
itsjohncs <johnsullivan.pem@gmail.com>
2022-08-15 16:35:51 -0700

    Added some tooltips.
    I'll add some more descriptions as I go on.

--- Primer.Latex/Runtime/Timeline/LatexTransitionClip.cs
+++ Primer.Latex/Runtime/Timeline/LatexTransitionClip.cs
@@ -9,10 +9,19 @@ namespace LatexRenderer.Timeline
 {
     public class LatexTransitionClip : PlayableAsset, ITimelineClipAsset
     {
+        [Tooltip(
+            "Parent object containing all the released SVG parts that are visible *before* the transition.")]
         public ExposedReference<Transform> before;
+
+        [Tooltip(
+            "Parent object containing all the released SVG parts that are visible *after* the transition.")]
         public ExposedReference<Transform> after;
 
+        [Tooltip("Child of `before` that will be used for alignment: see `afterAnchor`.")]
         public ExposedReference<Transform> beforeAnchor;
+
+        [Tooltip(
+            "Child of `after`. At the start of the transition, `after` will be moved such that the center of `afterAnchor` lines up exactly with the center of `beforeAnchor`. This allows you to keep the two expressions separated during editing.")]
         public ExposedReference<Transform> afterAnchor;
 
         public List<MorphTransition> morphTransitions;
