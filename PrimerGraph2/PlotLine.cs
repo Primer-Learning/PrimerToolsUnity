c3bf2257c1919b577081f4fd191ba5bdec66b892
Matías Quezada <matias@primerlearning.org>
2022-11-03 18:18:10 +0100

    PlotLines using Shapes library

--- PrimerGraph2/PlotLine.cs
+++ PrimerGraph2/PlotLine.cs
@@ -1,20 +1,24 @@
+using System.Collections.Generic;
 using Primer;
 using Primer.Timeline;
+using Shapes;
+using UnityEditor;
 using UnityEngine;
 
 public class PlotLine : ScrubbableAdapter
 {
+    public int resolution = 200;
     public int length = 10;
-    public float step = 0.1f;
-    public LineRenderer prefab;
+    // public float step = 0.1f;
+    public Polyline prefab;
 
     public ExposedReference<Transform> _parent;
     Transform parent => Resolve(_parent);
 
-    LineRenderer line;
+    Polyline line;
 
     public virtual float Curve(float x) {
-        if (x == 5) return 0;
+        if (x == 5f) return 0;
         return Mathf.Log(x + 1, 2);
     }
 
@@ -24,17 +28,19 @@ public class PlotLine : ScrubbableAdapter
             Prepare();
         }
 
-        var totalPointsCount = length * (1 / step);
+        var totalPointsCount = resolution;
+        var step = length / (float)totalPointsCount;
         var toRender = Mathf.RoundToInt(totalPointsCount * (float)args.time);
-        var points = new Vector3[toRender];
+        var points = new List<PolylinePoint>();
 
         for (var i = 0; i < toRender; i++) {
             var x = i * step;
-            points[i] = new Vector3(x, Curve(x), 0);
+            var vec = new Vector2(x, Curve(x));
+            points.Add(new PolylinePoint(vec));
         }
 
-        line.positionCount = toRender;
-        line.SetPositions(points);
+        line.points = points;
+        line.meshOutOfDate = true;
     }
 
     public override void Prepare() {
@@ -43,7 +49,7 @@ public class PlotLine : ScrubbableAdapter
 
     public override void Cleanup() {
         Debug.Log("CLEANUP");
-        line.gameObject.Dispose();
+        line?.gameObject?.Dispose();
     }
 
     public override void RegisterPreviewingProperties(PropertyRegistrar registrar) {
