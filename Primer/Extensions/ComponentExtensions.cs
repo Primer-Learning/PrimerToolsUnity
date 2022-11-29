b4422b037a22733bd061cf1ec252aa7ab420963a
Matías Quezada <matias@primerlearning.org>
2022-11-29 02:27:54 +0100

    Add component extension GetOrAddComponent

---
+++ Primer/Extensions/ComponentExtensions.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Primer
+{
+    public static class ComponentExtensions
+    {
+        public static T GetOrAddComponent<T>(this Component component) where T : Component =>
+            component.GetComponent<T>() ?? component.gameObject.AddComponent<T>();
+    }
+}
