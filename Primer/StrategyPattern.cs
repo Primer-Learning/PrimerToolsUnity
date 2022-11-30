d08a8fd5836cf0b9192c6a728f9b81009edbdd0a
Matías Quezada <matias@primerlearning.org>
2022-11-30 15:17:02 +0100

    Add documentation for StrategyPattern

--- Primer/StrategyPattern.cs
+++ Primer/StrategyPattern.cs
@@ -1,5 +1,29 @@
 namespace Primer
 {
+    /// <summary>
+    /// Extend this class to create a type that will be shown as a dropdown in Unity's inspector.
+    /// This dropdown will allow you to choose between all subtypes of your class.
+    /// </summary>
+    /// <example>
+    /// abstract class SomeNumber : StrategyPattern {
+    ///     public abstract int GetSomeNumber();
+    /// }
+    /// class NumberOne : SomeNumber {
+    ///     public override int GetSomeNumber() => 1;
+    /// }
+    /// class NumberTwo : SomeNumber {
+    ///     public override int GetSomeNumber() => 2;
+    /// }
+    /// class NumberThree : SomeNumber {
+    ///     public override int GetSomeNumber() => 3;
+    /// }
+    ///
+    /// class MyBehaviour : MonoBehaviour {
+    ///     // This will become a dropdown in Unity's inspector with NumberOne, NumberTwo and NumberThree
+    ///     [SerializeReference] SomeNumber strategy = new NumberOne();
+    ///     void Update() => Debug.Log(strategy.GetSomeNumber());
+    /// }
+    /// </example>
     public class StrategyPattern
     {
     }
