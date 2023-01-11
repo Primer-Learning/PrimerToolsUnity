d2569a1aac73794fb98839e9a1ede7184cd0e425
Matías Quezada <matias@primerlearning.org>
2023-01-11 18:21:56 +0100

    Return child generation logic into AxisRenderer

--- Primer.Axis/AxisRenderer.cs
+++ Primer.Axis/AxisRenderer.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using Primer.Animation;
+using TMPro;
 using UnityEngine;
 
 namespace Primer.Axis
@@ -50,19 +52,10 @@ namespace Primer.Axis
         private void OnValidate() => UpdateChildren();
 
 
-        #region Children management
-        private AxisLabel labelController;
-        private AxisArrows arrowsController;
-        private AxisTicks ticksController;
-
         public void UpdateChildren()
         {
             if (gameObject.IsPreset())
                 return;
-
-            labelController ??= new AxisLabel(this);
-            arrowsController ??= new AxisArrows(this);
-            ticksController ??= new AxisTicks(this);
 
             var children = new ChildrenModifier(transform) {
                 onCreate = x => x.GetPrimer().ScaleUpFromZero().Forget(),
@@ -76,12 +69,130 @@ namespace Primer.Axis
                 rod.localPosition = new Vector3(offset, 0f, 0f);
                 rod.localScale = new Vector3(length, thickness, thickness);
 
-                labelController.Update(children);
-                arrowsController.Update(children);
-                ticksController.Update(children);
+                UpdateLabel(children);
+                UpdateArrows(children);
+                UpdateTicks(children);
             }
 
             children.Apply();
+        }
+
+
+        #region UpdateLabel()
+        private PrimerText2 labelObject;
+
+        public void UpdateLabel(ChildrenModifier modifier)
+        {
+            labelObject = modifier.Next(labelObject)
+                .Called("Label")
+                .WithComponent<PrimerText2>();
+
+            var position = labelPosition switch {
+                AxisLabelPosition.Along => new Vector3(length / 2 + offset, -2 * ticLabelDistance, 0f),
+                AxisLabelPosition.End => new Vector3(length + offset + ticLabelDistance * 1.1f, 0f, 0f),
+                _ => Vector3.zero,
+            };
+
+            labelObject.text = label;
+            labelObject.alignment = TextAlignmentOptions.Midline;
+            labelObject.transform.localPosition = position + labelOffset;
+        }
+        #endregion
+
+
+        #region UpdateArrows()
+        private static readonly Quaternion originArrowRotation = Quaternion.Euler(0f, -90f, 0f);
+        private static readonly Quaternion endArrowRotation = Quaternion.Euler(0f, 90f, 0f);
+        private ArrowPresence? lastPresence;
+        private Transform originArrow;
+        private Transform endArrow;
+
+        public void UpdateArrows(ChildrenModifier modifier)
+        {
+            FindOrCreateArrows(modifier);
+
+            if (endArrow)
+                endArrow.transform.localPosition = new Vector3(length + offset, 0f, 0f);
+
+            if (originArrow)
+                originArrow.transform.localPosition = new Vector3(offset, 0f, 0f);
+        }
+
+        private void FindOrCreateArrows(ChildrenModifier modifier)
+        {
+            if (arrowPresence == lastPresence) {
+                modifier.NextMustBe(originArrow);
+                modifier.NextMustBe(endArrow);
+                return;
+            }
+
+            lastPresence = arrowPresence;
+
+            if (arrowPresence == ArrowPresence.Neither) {
+                endArrow = null;
+                originArrow = null;
+                return;
+            }
+
+            endArrow = modifier.Next(endArrow)
+                .Called("End Arrow")
+                .Initialize(x => x.rotation = endArrowRotation)
+                .InstantiatedFrom(arrowPrefab);
+
+            if (arrowPresence == ArrowPresence.Positive) {
+                originArrow = null;
+                return;
+            }
+
+            originArrow = modifier.Next(originArrow)
+                .Called("Origin Arrow")
+                .Initialize(x => x.rotation = originArrowRotation)
+                .InstantiatedFrom(arrowPrefab);
+        }
+        #endregion
+
+
+        #region UpdateTicks()
+        public void UpdateTicks(ChildrenModifier modifier)
+        {
+            if (!showTics || ticStep <= 0)
+                return;
+
+            var expectedTicks = manualTics.Count != 0
+                ? manualTics
+                : CalculateTics();
+
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+            if ((maxTics > 0) && (expectedTicks.Count > maxTics)) {
+                // TODO: reduce amount of tics in a smart way
+                expectedTicks = expectedTicks.Take(maxTics).ToList();
+            }
+
+            foreach (var data in expectedTicks) {
+                var tick = modifier.Next()
+                    .Called($"Tick {data.label}")
+                    .InstantiatedFrom(tickPrefab);
+
+                tick.label = data.label;
+                tick.transform.localPosition = new Vector3(data.value * positionMultiplier, 0, 0);
+            }
+        }
+
+        private List<TicData> CalculateTics()
+        {
+            var calculated = new List<TicData>();
+            var step = Mathf.Round(ticStep * 100) / 100;
+
+            if (step <= 0)
+                return calculated;
+
+            for (var i = step; i <= max; i += step)
+                calculated.Add(new TicData(i));
+
+            for (var i = -step; i >= min; i -= step)
+                calculated.Add(new TicData(i));
+
+            return calculated;
         }
         #endregion
     }
