9ea496611d912768b78c5492e8f824ee43da533b
Matías Quezada <matias@primerlearning.org>
2023-01-17 14:04:49 +0100

    Update PrimerText2 to use Odin Inspector

--- Primer/Internals/PrimerText2.cs
+++ Primer/Internals/PrimerText2.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 
@@ -7,16 +8,42 @@ namespace Primer
     [RequireComponent(typeof(TextMeshPro))]
     public class PrimerText2 : MonoBehaviour
     {
-        [Multiline]
-        public string text = "";
-        public float fontSize = 2;
-        public Color color = Color.white;
-        public TextAlignmentOptions alignment = TextAlignmentOptions.Center;
-        public Vector3 orientation = Vector3.forward;
-
-
         private TextMeshPro meshCache;
         private TextMeshPro textMeshPro => meshCache ??= GetComponent<TextMeshPro>();
+
+
+        public bool forceOrientation = true;
+
+        [EnableIf(nameof(forceOrientation))]
+        public Vector3 orientation = Vector3.forward;
+
+        [ShowInInspector]
+        [HideLabel]
+        [PropertyOrder(-1)]
+        [PropertySpace(SpaceBefore = 16, SpaceAfter = 16)]
+        [MultiLineProperty(5)]
+        public string text {
+            get => textMeshPro.text;
+            set => textMeshPro.text = value;
+        }
+
+        [ShowInInspector]
+        public float fontSize {
+            get => textMeshPro.fontSize;
+            set => textMeshPro.fontSize = value;
+        }
+
+        [ShowInInspector]
+        public TextAlignmentOptions alignment {
+            get => textMeshPro.alignment;
+            set => textMeshPro.alignment = value;
+        }
+
+        [ShowInInspector]
+        public Color color {
+            get => textMeshPro.color;
+            set => textMeshPro.color = value;
+        }
 
 
         private float numericValue;
@@ -25,27 +52,14 @@ namespace Primer
             set {
                 numericValue = value;
                 text = Presentation.FormatNumber(value);
-                textMeshPro.text = text;
             }
         }
 
 
-        private void OnValidate() => UpdateMesh();
-
         private void Update()
         {
-            UpdateMesh();
-            transform.LookAt(transform.position + orientation);
-        }
-
-
-        private void UpdateMesh()
-        {
-            var mesh = textMeshPro;
-            mesh.text = text;
-            mesh.color = color;
-            mesh.fontSize = fontSize;
-            mesh.alignment = alignment;
+            if (forceOrientation)
+                transform.LookAt(transform.position + orientation);
         }
     }
 }
