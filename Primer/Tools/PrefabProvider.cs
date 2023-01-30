a1598c51a3f9e6474e66a5a35b7501799997097c
Matías Quezada <matias@primerlearning.org>
2023-01-30 17:20:13 +0100

    Fix PrefabProvider serialization issue

--- Primer/Tools/PrefabProvider.cs
+++ Primer/Tools/PrefabProvider.cs
@@ -7,8 +7,6 @@ using Vector3 = UnityEngine.Vector3;
 
 namespace Primer
 {
-
-
     public interface IPrefabProvider<out T> where T : Component
     {
         bool hasChanges { get; set; }
@@ -46,105 +44,96 @@ namespace Primer
                 if (Equals(stored, value))
                     return;
 
-                stored = value;
-                hasChanges = true;
-
-                Nullable<Vector3> a = null;;
-
                 if (value == null) {
-                    _position = null;
-                    _rotation = null;
-                    _scale = null;
+                    ResetPosition();
+                    ResetRotation();
+                    ResetScale();
                     target = null;
                 }
                 else {
                     target = value.transform;
                 }
+
+                stored = value;
+                hasChanges = true;
             }
         }
 
-        // [HorizontalGroup("details", 70, LabelWidth = 60)]
-
-        // [ShowInInspector]
-        // [ShowIf("$stored")]
-        // [VerticalGroup("details/left")]
-        // [HideLabel]
-        // [InlineEditor(
-        //     InlineEditorModes.SmallPreview,
-        //     Expanded = true,
-        //     PreviewHeight = 70,
-        //     ObjectFieldMode = InlineEditorObjectFieldModes.Hidden
-        // )]
-        // private T preview => value;
         #endregion
 
 
         #region public Vector3 position = value.localPosition ?? Vector3.zero;
         [SerializeReference]
-        private Vector3? _position;
+        private Vector3 _position;
+        public bool hasPosition = false;
 
         [ShowInInspector]
         [ShowIf("$value")]
-        // [VerticalGroup("details/right")]
         [LabelWidth(60)]
         [InlineButton(nameof(ResetPosition), SdfIconType.Eraser, label: "")]
         public Vector3 position {
-            get => _position ?? (target != null ? target.localPosition : Vector3.zero);
+            get => hasPosition || target == null ? _position : target.localPosition;
             set {
                 hasChanges = true;
+                hasPosition = true;
                 _position = value;
             }
         }
 
         public void ResetPosition() {
             hasChanges = true;
-            _position = null;
+            hasPosition = false;
+            _position = default(Vector3);
         }
         #endregion
 
         #region public Quaternion rotation = value.localRotation ?? Quaternion.identity;
         [SerializeReference]
-        private Quaternion? _rotation;
+        private Quaternion _rotation;
+        public bool hasRotation = false;
 
         [ShowInInspector]
         [ShowIf("$value")]
-        // [VerticalGroup("details/right")]
         [LabelWidth(60)]
         [InlineButton(nameof(ResetRotation), SdfIconType.Eraser, label: "")]
         public Quaternion rotation {
-            get => _rotation ?? (target != null ? target.localRotation : Quaternion.identity);
+            get => hasRotation || target == null ? _rotation : target.localRotation;
             set {
                 hasChanges = true;
+                hasRotation = true;
                 _rotation = value;
             }
         }
 
         public void ResetRotation() {
             hasChanges = true;
-            _rotation = null;
+            hasRotation = false;
+            _rotation = default(Quaternion);
         }
         #endregion
 
         #region public Vector3 scale = value.localScale ?? Vector3.one;
         [SerializeReference]
-        private Vector3? _scale;
+        private Vector3 _scale = Vector3.one;
+        public bool hasScale = false;
 
         [ShowInInspector]
         [ShowIf("$value")]
-        // [VerticalGroup("details/right")]
         [LabelWidth(60)]
         [InlineButton(nameof(ResetScale), SdfIconType.Eraser, label: "")]
         public Vector3 scale {
-            get => _scale ?? (target != null ? target.localScale : Vector3.one);
+            get => hasScale || target == null ? _scale : target.localScale;
             set {
                 hasChanges = true;
+                hasScale = true;
                 _scale = value;
             }
         }
 
         public void ResetScale() {
             hasChanges = true;
-            _scale = null;
+            hasScale = false;
+            _scale = Vector3.one;
         }
         #endregion
 
@@ -160,14 +149,14 @@ namespace Primer
         {
             var transform = child.transform;
 
-            if (_scale is not null)
-                transform.localScale = _scale.Value;
+            if (hasScale)
+                transform.localScale = _scale;
 
-            if (_rotation is not null)
-                transform.rotation = _rotation.Value;
+            if (hasRotation)
+                transform.rotation = _rotation;
 
-            if (_position is not null)
-                transform.localPosition = _position.Value;
+            if (hasPosition)
+                transform.localPosition = _position;
         }
 
 
