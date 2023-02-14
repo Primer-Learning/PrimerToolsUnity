22bde8375664c24fba8873fa4e9c5ca2d11e1d42
Matías Quezada <matias@primerlearning.org>
2023-02-14 19:21:47 +0100

    Create Async class for async managers that don't extend MonoBehaviour

--- Primer.Timeline/Internals/AsyncMonoBehaviour.cs
+++ Primer.Timeline/Internals/AsyncMonoBehaviour.cs
@@ -3,6 +3,28 @@ using UnityEngine;
 
 namespace Primer.Timeline
 {
+    // These two classes are identical, except one extends MonoBehaviour and the other doesn't
+
+    public class Async
+    {
+        protected static async UniTask Milliseconds(int milliseconds)
+        {
+            if (Application.isPlaying)
+                await UniTask.Delay(milliseconds);
+        }
+
+        protected static async UniTask Seconds(float seconds)
+        {
+            if (Application.isPlaying)
+                await UniTask.Delay(Mathf.RoundToInt(seconds * 1000));
+        }
+
+        protected static async UniTask Parallel(params UniTask[] processes)
+        {
+            await UniTask.WhenAll(processes);
+        }
+    }
+
     public class AsyncMonoBehaviour : MonoBehaviour
     {
         protected static async UniTask Milliseconds(int milliseconds)
