b4215b56f511ebfaa26f797308dc3f2242cbb0ed
Matías Quezada <matias@primerlearning.org>
2023-04-04 12:45:40 +0200

    Add `afterUpdate` hook to ObservableTween

--- Primer.Animation/Internals/ObservableTween.cs
+++ Primer.Animation/Internals/ObservableTween.cs
@@ -9,6 +9,8 @@ namespace Primer.Animation
 
         public Action beforePlay { get; init; }
         public Action onComplete { get; init; }
+        // public Action<float> beforeUpdate { get; init; }
+        public Action<float> afterUpdate { get; init; }
 
         public override void Evaluate(float t)
         {
@@ -17,7 +19,9 @@ namespace Primer.Animation
                 state = State.Playing;
             }
 
+            // beforeUpdate?.Invoke(t);
             base.Evaluate(t);
+            afterUpdate?.Invoke(t);
 
             if (state == State.Playing && t >= 1) {
                 onComplete?.Invoke();
@@ -28,16 +32,24 @@ namespace Primer.Animation
 
     public static class ObservableTweenExtensions
     {
-        public static ObservableTween Observe(this Tween tween, Action beforePlay = null, Action onComplete = null)
-        {
+        public static ObservableTween Observe(
+            this Tween tween,
+            Action beforePlay = null,
+            Action onComplete = null,
+            // Action<float> beforeUpdate = null,
+            Action<float> afterUpdate = null
+        ) {
             return new ObservableTween(tween.lerp) {
                 // all of Tween fields should be copied here
                 easeMethod = tween.easeMethod,
                 delay = tween.delay,
                 isCalculated = tween.isCalculated,
                 ms = tween.ms,
+                // listeners
                 beforePlay = beforePlay,
                 onComplete = onComplete,
+                // beforeUpdate = beforeUpdate,
+                afterUpdate = afterUpdate,
             };
         }
     }
