9362d3281dab62e1201e6a4aa4f4f4dca6fff4a3
Justin Helps <justin.r.helps@gmail.com>
2023-04-07 13:41:40 -0500

    Add RotateTo transform extension.

--- Primer.Animation/Extensions/TransformExtensions.cs
+++ Primer.Animation/Extensions/TransformExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Primer.Animation
@@ -38,6 +39,28 @@ namespace Primer.Animation
             return initial == newPosition
                 ? Tween.noop
                 : new Tween(t => transform.localPosition = Vector3.Lerp(initial, newPosition, t));
+        }
+        
+        public static Tween RotateTo(this Transform transform, Quaternion newRotation)
+        {
+            var initial = transform.localRotation;
+
+            try
+            {
+                return new Tween(t => transform.localRotation = Quaternion.Lerp(initial, newRotation, t));
+            }
+            catch
+            {
+                Debug.LogWarning("Tween failed in RotateTo. Quaternions may be too close.");
+                return Tween.noop;
+            }
+            
+            // The code below did not work.
+            // GPT 4 says quaternion equality check is unreliable, and that Unity does not allow lerping of quaternions
+            // that are very close together, for some reason.
+            return initial == newRotation
+                ? Tween.noop
+                : new Tween(t => transform.localRotation = Quaternion.Lerp(initial, newRotation, t));
         }
     }
 }
