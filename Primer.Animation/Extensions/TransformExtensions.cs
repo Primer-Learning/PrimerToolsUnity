6a7fbc740cc7f01392c0f0406f6910fbe5d9eb56
Matías Quezada <matias@primerlearning.org>
2023-04-13 15:58:28 +0200

    Fix error handling in Transform.RotateTo() extension method

--- Primer.Animation/Extensions/TransformExtensions.cs
+++ Primer.Animation/Extensions/TransformExtensions.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Threading;
-using Cysharp.Threading.Tasks;
-using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Primer.Animation
@@ -31,6 +27,7 @@ namespace Primer.Animation
                 ? Tween.noop
                 : new Tween(t => transform.localPosition = Vector3.Lerp(initial, newPosition, t));
         }
+
         public static Tween MoveBy(this Transform transform, Vector3 displacement)
         {
             var initial = transform.localPosition;
@@ -40,27 +37,27 @@ namespace Primer.Animation
                 ? Tween.noop
                 : new Tween(t => transform.localPosition = Vector3.Lerp(initial, newPosition, t));
         }
-        
+
         public static Tween RotateTo(this Transform transform, Quaternion newRotation)
         {
             var initial = transform.localRotation;
+            var hasFailed = false;
 
-            try
-            {
-                return new Tween(t => transform.localRotation = Quaternion.Lerp(initial, newRotation, t));
-            }
-            catch
-            {
-                Debug.LogWarning("Tween failed in RotateTo. Quaternions may be too close.");
-                return Tween.noop;
-            }
-            
-            // The code below did not work.
-            // GPT 4 says quaternion equality check is unreliable, and that Unity does not allow lerping of quaternions
-            // that are very close together, for some reason.
-            return initial == newRotation
-                ? Tween.noop
-                : new Tween(t => transform.localRotation = Quaternion.Lerp(initial, newRotation, t));
+            return new Tween(t => {
+                if (hasFailed)
+                    return;
+
+                try {
+                    transform.localRotation = Quaternion.Lerp(initial, newRotation, t);
+                }
+                catch {
+                    // GPT 4 says quaternion equality check is unreliable, and that Unity does not allow lerping of quaternions
+                    // that are very close together, for some reason.
+                    Debug.LogWarning("Tween failed in RotateTo. Quaternions may be too close.");
+                    hasFailed = true;
+                    transform.localRotation = newRotation;
+                }
+            });
         }
     }
 }
