3cdaaae284a8cf4f6a1bb84ae1414234b714bc23
Justin Helps <justin.r.helps@gmail.com>
2023-05-24 19:24:04 -0500

    Add EaseWithAccelerationPeriod for long animations that should mostly have a constant speed, except some easing at the beginning and end to avoid jerky movement.

--- Primer.Animation/Internals/IEasing.cs
+++ Primer.Animation/Internals/IEasing.cs
@@ -143,4 +143,65 @@ namespace Primer.Animation
             return -(endVal - startVal) * t * t + endVal;
         }
     }
+    
+    public class EaseWithAccelerationPeriod : IEasing
+    {
+        private float AccelerationPeriod { get; set; }  // The duration of the acceleration and deceleration period
+
+        public EaseWithAccelerationPeriod(float accelerationPeriod)
+        {
+            if (accelerationPeriod < 0 || accelerationPeriod > 0.5)
+            {
+                Debug.LogError("Acceleration period must be in the range [0, 0.5]");
+            }
+
+            this.AccelerationPeriod = accelerationPeriod;
+        }
+
+        public float Evaluate(float t)
+        {
+            return EvaluateEasing(0, 1, t);
+        }
+
+        private float EvaluateEasing(float startVal, float endVal, float t)
+        {
+            // Check if the parameters are within acceptable ranges
+            if (t < 0 || t > 1)
+            {
+                Debug.LogError("Time t must be in the range [0, 1]");
+            }
+            
+            // Reference to Desmos calculator file: https://www.desmos.com/calculator/3gbx5s3swk
+            // This was solved by knowing the equations in the if blocks must have the form they do.
+            // Then solving for these two parameters when the functions are equal and tangent at t = AccelerationPeriod
+            
+            float v_max = 1 / (1 - AccelerationPeriod);
+            float accelerationMagnitude = 1 / (2 * AccelerationPeriod * (1 - AccelerationPeriod));
+
+            if (t <= 0)
+            {
+                return startVal;
+            }
+            else if (t < AccelerationPeriod)
+            {
+                // Acceleration phase (parabola)
+                return (endVal - startVal) * accelerationMagnitude * (t * t) + startVal;
+            }
+            else if (t < 1 - AccelerationPeriod)
+            {
+                // Constant speed phase (line)
+                return (endVal - startVal) * (0.5f + v_max * (t - 0.5f)) + startVal;
+            }
+            else if (t <= 1)
+            {
+                // Deceleration phase (parabola)
+                t = t - 1; // Change this to simplify the next line
+                return (endVal - startVal) * (- accelerationMagnitude * (t * t) + 1) + startVal;
+            }
+            else
+            {
+                return endVal;
+            }
+        }
+    }
 }
