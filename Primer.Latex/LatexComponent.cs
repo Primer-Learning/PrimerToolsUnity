b1c59c215ae94ecfabe70d0059fe66a9adbe68e5
Matías Quezada <matias@primerlearning.org>
2023-05-26 16:07:19 +0200

    Load frames will show an dialog when done

--- Primer.Latex/LatexComponent.cs
+++ Primer.Latex/LatexComponent.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using Primer.Animation;
 using Sirenix.OdinInspector;
--- Primer.Timeline/Editor/Actions/LoadFrames.cs
+++ Primer.Timeline/Editor/Actions/LoadFrames.cs
@@ -1,5 +1,6 @@
 using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
+using UnityEditor;
 using UnityEditor.Timeline;
 using UnityEditor.Timeline.Actions;
 using UnityEngine.Playables;
@@ -7,7 +8,7 @@ using UnityEngine.Playables;
 namespace Primer.Timeline.Editor
 {
     [UsedImplicitly]
-    [MenuEntry("Start loading frames", priority: 9005)]
+    [MenuEntry("Load frames from cursor", priority: 9005)]
     public class LoadFrames : TimelineAction
     {
         public static bool isPlaying = false;
@@ -26,26 +27,46 @@ namespace Primer.Timeline.Editor
             return true;
         }
 
-        public static async void StartPlaying(PlayableDirector playableDirector, float? from = null)
+        public static async void StartPlaying(PlayableDirector playableDirector, float? from = null,
+            bool showDialogWhenDone = true)
         {
             isPlaying = true;
 
             if (from.HasValue)
                 await PlayControl.PlayDirectorAt(playableDirector, from.Value);
 
+            RequestConstantUpdates();
+
             try {
-                while (isPlaying && playableDirector.time < playableDirector.duration) {
-                    await PlayControl.PlayDirectorAt(playableDirector, (float)playableDirector.time + STEP);
+                while (isPlaying) {
+                    await PlayControl.PlayDirectorAt(playableDirector, playableDirector.time + STEP);
                     await UniTask.Delay(1);
+
+                    if (playableDirector.time < playableDirector.duration)
+                        continue;
+
+                    if (showDialogWhenDone)
+                        EditorUtility.DisplayDialog("Load frames", "All frames loaded", "Ok");
+
+                    break;
                 }
-            } finally {
+            }
+            finally {
                 isPlaying = false;
+            }
+        }
+
+        private static async void RequestConstantUpdates()
+        {
+            while (isPlaying) {
+                EditorApplication.QueuePlayerLoopUpdate();
+                await UniTask.Delay(10);
             }
         }
     }
 
     [UsedImplicitly]
-    [MenuEntry("Load frames from start", priority: 9005)]
+    [MenuEntry("Load frames from start", priority: 9004)]
     public class LoadFramesFromStart : TimelineAction
     {
         public override ActionValidity Validate(ActionContext context)
