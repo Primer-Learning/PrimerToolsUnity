5ed5fb1a0568553479ecec781e42e6bbf05936cb
Matías Quezada <matias@primerlearning.org>
2023-05-26 12:56:41 +0200

    Add labels above bars in BarPlot

--- Primer.Graph/Internals/BarPlot.cs
+++ Primer.Graph/Internals/BarPlot.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Primer.Animation;
+using Primer.Latex;
 using Primer.Shapes;
 using Primer.Timeline;
 using Shapes;
@@ -15,13 +16,27 @@ namespace Primer.Graph
     {
         public static Color defaultColor = new Vector4(62, 126, 160, 255) / 255;
 
+        private Container _barsContainer;
+        private Container barsContainer => _barsContainer ??= new Container("Plotted bars", graph.domain);
+
+        private Container _labelsContainer;
+        private Container labelsContainer {
+            get {
+                _labelsContainer ??= new Container("Plotted bars labels", graph);
+                _labelsContainer.SetScale(0.1f);
+                return _labelsContainer;
+            }
+        }
+
         private Graph graphCache;
         private Graph graph => graphCache ??= GetComponent<Graph>();
-
 
         public BarData this[int index] => GetBar(index);
         public BarData this[string name] => GetBar(name);
 
+        public Func<BarData, string> barLabels = null;
+
+        #region float barWidth;
         [SerializeField, HideInInspector]
         private float _barWidth = 1f;
         [ShowInInspector]
@@ -29,7 +44,9 @@ namespace Primer.Graph
             get => _barWidth;
             set => Meta.ReactiveProp(ref _barWidth, value, UpdateBars);
         }
+        #endregion
 
+        #region float cornerRadius;
         [SerializeField, HideInInspector]
         private float _cornerRadius = 0.25f;
         [ShowInInspector]
@@ -37,7 +54,9 @@ namespace Primer.Graph
             get => _cornerRadius;
             set => Meta.ReactiveProp(ref _cornerRadius, value, UpdateBars);
         }
+        #endregion
 
+        #region Vector3 offset;
         [SerializeField, HideInInspector]
         private Vector3 _offset = Vector3.zero;
         [ShowInInspector]
@@ -45,7 +64,19 @@ namespace Primer.Graph
             get => _offset;
             set => Meta.ReactiveProp(ref _offset, value, UpdateBars);
         }
+        #endregion
 
+        #region Vector3 labelOffset;
+        [SerializeField, HideInInspector]
+        private Vector3 _labelOffset = Vector3.zero;
+        [ShowInInspector]
+        public Vector3 labelOffset {
+            get => _labelOffset;
+            set => Meta.ReactiveProp(ref _labelOffset, value, UpdateBars);
+        }
+        #endregion
+
+        #region List<BarData> bars;
         [SerializeField, HideInInspector]
         private List<BarData> _bars = new();
         [ShowInInspector]
@@ -56,6 +87,7 @@ namespace Primer.Graph
             get => _bars;
             set => Meta.ReactiveProp(ref _bars, value, UpdateBars);
         }
+        #endregion
 
 
         public void SetDefaults()
@@ -247,45 +279,69 @@ namespace Primer.Graph
         #endregion
 
 
-        // public static Dictionary<float, string> GenerateIntegerCategories(
-        //     int num,
-        //     int min = 0,
-        //     int step = 1
-        // ) {}
-
-
         [Button]
         public void UpdateBars()
         {
-            var container = GetContainer();
-            var children = new ChildrenDeclaration(container);
+            var container = barsContainer;
+            container.Reset();
+            container.localScale = Vector3.one;
+            container.localPosition = Vector3.zero;
+
+            labelsContainer.Reset();
 
             for (var i = 0; i < bars.Count; i++) {
                 var data = bars[i];
-
-                var bar = children.Next<Rectangle>(
-                    name: data.name ?? $"Bar {i}",
-                    init: x => {
-                        x.Type = Rectangle.RectangleType.RoundedSolid;
-                        x.CornerRadiusMode = Rectangle.RectangleCornerRadiusMode.PerCorner;
-                        x.Pivot = RectPivot.Corner;
-                    }
-                );
-
-                bar.Color = data.color;
-                bar.Width = barWidth;
-                bar.Height = data.value;
-
-                bar.transform.localPosition = new Vector3(i  + (1 - barWidth) / 2, 0, 0) + offset;
-                bar.transform.localScale = Vector3.one;
-                bar.CornerRadii = new Vector4(0, cornerRadius, cornerRadius, 0);
+                var bar = CreateBar(container, data, i);
+                var label = barLabels is null ? null : CreateBarLabel(bar, data);
 
                 data.onNameChange = UpdateIfError<string>(newName => bar.gameObject.name = newName);
                 data.onColorChange = UpdateIfError<Color>(newColor => bar.Color = newColor);
-                data.onValueChange = UpdateIfError<float>(newValue => bar.Height = newValue);
+                data.onValueChange = UpdateIfError<float>(newValue => {
+                    bar.Height = newValue;
+                    label?.Process(barLabels(data));
+                });
             }
 
-            children.Apply();
+            labelsContainer.Purge();
+            container.Purge();
+        }
+
+        private LatexComponent CreateBarLabel(Rectangle bar, BarData data)
+        {
+            var domain = graph.domain;
+
+            var follower = labelsContainer.AddFollower(bar);
+            follower.component.useGlobalSpace = true;
+
+            follower.component.getter = () => {
+                var barMiddleTop = new Vector3(barWidth / 2, data.value);
+                var domainPosition = bar.transform.localPosition + barMiddleTop;
+                // return domain.TransformPoint(domainPosition);
+                var graphSpace = domain.localScale.ElementWiseMultiply(domainPosition);
+                return graph.transform.TransformPoint(graphSpace + labelOffset);
+            };
+
+            var label = follower.AddLatex(barLabels(data), $"{bar.name} label");
+            label.transform.SetDefaults();
+            return label;
+        }
+
+        private Rectangle CreateBar(Container container, BarData data, int i)
+        {
+            var bar = container.Add<Rectangle>(data.name ?? $"Bar {i}");
+            PrimerTimeline.MarkAsEphemeral(bar);
+
+            bar.Type = Rectangle.RectangleType.RoundedSolid;
+            bar.CornerRadiusMode = Rectangle.RectangleCornerRadiusMode.PerCorner;
+            bar.Pivot = RectPivot.Corner;
+            bar.Color = data.color;
+            bar.Width = barWidth;
+            bar.Height = data.value;
+
+            bar.transform.localPosition = new Vector3(i + (1 - barWidth) / 2, 0, 0) + offset;
+            bar.transform.localScale = Vector3.one;
+            bar.CornerRadii = new Vector4(0, cornerRadius, cornerRadius, 0);
+            return bar;
         }
 
         private Action<T> UpdateIfError<T>(Action<T> handler)
@@ -299,21 +355,6 @@ namespace Primer.Graph
                     UpdateBars();
                 }
             };
-        }
-
-        private Transform barsContainer;
-        private Transform GetContainer()
-        {
-            if (barsContainer == null)
-                barsContainer = new GameObject("Plotted bars").transform;
-
-            if (barsContainer.parent != graph.domain)
-                barsContainer.parent = graph.domain;
-
-            barsContainer.localScale = Vector3.one;
-            barsContainer.localPosition = Vector3.zero;
-            PrimerTimeline.MarkAsEphemeral(barsContainer);
-            return barsContainer;
         }
     }
 }
