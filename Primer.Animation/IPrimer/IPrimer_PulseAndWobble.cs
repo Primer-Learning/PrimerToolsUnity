fed56e3b250fb32e73c455ac2dd93d831ac997d8
Justin Helps <justin.r.helps@gmail.com>
2023-06-09 16:05:23 -0500

    Return the tracking of currentRotation to PulseAndWobble. There was probably a way to do this while keeping the more elegant new implementation, but I decided for this one to take the path I knew to returned functionality.

--- Primer.Animation/IPrimer/IPrimer_PulseAndWobble.cs
+++ Primer.Animation/IPrimer/IPrimer_PulseAndWobble.cs
@@ -19,43 +19,32 @@ namespace Primer.Animation
             // making it look more natural.
 
             var transform = self.component.transform;
-            var pulse = self.Pulse(sizeFactor, attack, hold, decay);
+            var pulse = transform.Pulse(sizeFactor, attack, hold, decay);
 
             // State tracking vars
-            var totalDuration = attack + hold + decay;
             var initialRotation = transform.localRotation;
+            var totalDuration = attack + hold + decay;
+            // Need to keep track of this and feed to the RotateToCalls as the initial rotation,
+            // since the Tweens called in series don't know about each other.
+            var currentRotation = initialRotation;
+
             var quarterPeriod = wobbleCyclePeriod / 4; // Easier to work with
+            var wobbleTweens = new List<Tween>();
+            var targetRotation = Quaternion.Euler(0, 0, -wobbleAngle) * initialRotation;
+            wobbleTweens.Add(transform.RotateTo(targetRotation) with {duration = quarterPeriod, easing = new QuadraticEasing()});
+            totalDuration -= quarterPeriod;
+            currentRotation = targetRotation;
 
-            var wobbleTweens = new List<Tween> {
-                transform.RotateBy(z: -wobbleAngle) with {
-                    duration = quarterPeriod,
-                    easing = new QuadraticEasing(),
-                },
-            };
-
-            var angleMultiplier = 2;
-
-            for (
-                totalDuration -= quarterPeriod;
-                totalDuration >= 3 * quarterPeriod;
-                totalDuration -= 2 * quarterPeriod
-            ) {
-                wobbleTweens.Add(
-                    transform.RotateBy(z: wobbleAngle * angleMultiplier) with {
-                        duration = 2 * quarterPeriod,
-                        easing = new QuadraticEasing(),
-                    }
-                );
-
+            var angleMultiplier = 1;
+            while (totalDuration >= 3 * quarterPeriod)
+            {
+                targetRotation = Quaternion.Euler(0, 0, wobbleAngle * angleMultiplier) * initialRotation;
+                wobbleTweens.Add(transform.RotateTo(targetRotation, initialRotation: currentRotation) with {duration = 2 * quarterPeriod, easing = new QuadraticEasing()});
+                currentRotation = targetRotation;
+                totalDuration -= 2 * quarterPeriod;
                 angleMultiplier *= -1;
             }
-
-            wobbleTweens.Add(
-                transform.RotateTo(initialRotation) with {
-                    duration = quarterPeriod,
-                    easing = new QuadraticEasing(),
-                }
-            );
+            wobbleTweens.Add(transform.RotateTo(initialRotation, initialRotation: currentRotation) with {duration = quarterPeriod, easing = new QuadraticEasing()});
 
             return Tween.Parallel(
                 Tween.Series(wobbleTweens.ToArray()),
