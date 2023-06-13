bb162cbc14c5cb35e331bb9f85cc2f33487eb3c0
Matías Quezada <matias@primerlearning.org>
2023-06-13 19:26:43 +0200

    Remove complex timeline track mixers
    This also removes GraphLine, GraphPoint and GraphSurface

--- Primer.Graph/Editor/GraphCreateUtility.cs
+++ Primer.Graph/Editor/GraphCreateUtility.cs
@@ -1,7 +1,5 @@
-using System;
 using Primer.Editor;
 using UnityEditor;
-using UnityEngine;
 
 namespace Primer.Graph.Editor
 {
@@ -9,49 +7,5 @@ namespace Primer.Graph.Editor
     {
         [MenuItem("GameObject/Primer/Graph", false, CreateUtility.PRIORITY)]
         public static void Graph() => CreateUtility.Prefab("Graph2");
-
-        [MenuItem("GameObject/Primer/Graph Points", true)]
-        [MenuItem("GameObject/Primer/Graph Line", true)]
-        [MenuItem("GameObject/Primer/Graph Surface", true)]
-        public static bool ValidateIsGraph(MenuCommand command) =>
-            // Selection can only be null or a live game object in the hierarchy panel
-            // it can never be false null, so we disable this rule on this file
-            // ReSharper disable once Unity.NoNullPropagation
-            Selection.activeGameObject?.GetComponent<Graph>() is not null;
-
-        [MenuItem("GameObject/Primer/Graph Points", false, CreateUtility.PRIORITY)]
-        public static void Points(MenuCommand command) {
-            var graph = GetGraph(command);
-            var point = CreateUtility.Prefab("GraphPoints", graph.domain);
-            point.transform.localPosition = Vector3.zero;
-        }
-
-        [MenuItem("GameObject/Primer/Graph Line", false, CreateUtility.PRIORITY)]
-        public static void Line(MenuCommand command) {
-            var graph = GetGraph(command);
-            var line = CreateUtility.Prefab("GraphLine", graph.domain);
-            line.transform.localPosition = Vector3.zero;
-        }
-
-        [MenuItem("GameObject/Primer/Graph Surface", false, CreateUtility.PRIORITY)]
-        public static void Surface(MenuCommand command) {
-            var graph = GetGraph(command);
-            var line = CreateUtility.Prefab("GraphSurface", graph.domain);
-            line.transform.localPosition = Vector3.zero;
-        }
-
-        static Graph GetGraph(MenuCommand command) {
-            if (command.context is not GameObject go) {
-                throw new Exception("This should never happen");
-            }
-
-            var graph = go.GetComponent<Graph>();
-
-            if (graph is null) {
-                throw new Exception("Create graph surface command should only be executed on a graph");
-            }
-
-            return graph;
-        }
     }
 }
