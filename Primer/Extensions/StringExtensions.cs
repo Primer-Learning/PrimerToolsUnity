cd44936e7b6dc4ce99c320d7021de08272367290
Matías Quezada <matias@primerlearning.org>
2023-06-19 18:40:27 +0200

    intEnumerable.Join(", ")

--- Primer/Extensions/StringExtensions.cs
+++ Primer/Extensions/StringExtensions.cs
@@ -1,9 +1,15 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Primer
 {
     public static class StringExtensions
     {
+        public static string Join<T>(this IEnumerable<T> enumerable, string separator)
+        {
+            return string.Join(separator, enumerable.Select(x => x.ToString()));
+        }
+
         public static string Join(this IEnumerable<string> strings, string separator)
         {
             return string.Join(separator, strings);
