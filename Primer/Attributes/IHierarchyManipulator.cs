9de59d16c06b4979bc6d2e602b44baeeb3d45335
Matías Quezada <matias@primerlearning.org>
2023-06-21 11:41:32 +0200

    Create Primer/Attributes folder

--- Primer/Attributes/IHierarchyManipulator.cs
+++ Primer/Attributes/IHierarchyManipulator.cs
@@ -1,5 +1,8 @@
 namespace Primer
 {
+    /// <summary>
+    ///  This interface will add the relevant attributes to the implementations of the method in it.
+    /// </summary>
     public interface IHierarchyManipulator
     {
         public void UpdateChildren();
