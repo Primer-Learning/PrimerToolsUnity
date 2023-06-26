d4eb6d591343f12adf2859d90a3333be4c42cc03
Matías Quezada <matias@primerlearning.org>
2023-06-26 15:46:23 +0200

    Fix crash in Primer.Prefab

--- Primer/Utilities/Prefab.cs
+++ Primer/Utilities/Prefab.cs
@@ -6,7 +6,7 @@ namespace Primer
     public static class Prefab
     {
         private static readonly Dictionary<string, Component> cachedPrefabs = new();
-        private static readonly Cleaner cleaner = new Cleaner();
+        // private static readonly Cleaner cleaner = new Cleaner();
 
         public static GameObject Get(string prefabName)
             => Get<Transform>(prefabName).gameObject;
@@ -22,14 +22,14 @@ namespace Primer
             return prefab;
         }
 
-        private sealed class Cleaner
-        {
-            ~Cleaner()
-            {
-                foreach (var prefab in cachedPrefabs.Values) {
-                    Resources.UnloadAsset(prefab);
-                }
-            }
-        }
+        // private sealed class Cleaner
+        // {
+        //     ~Cleaner()
+        //     {
+        //         foreach (var prefab in cachedPrefabs.Values) {
+        //             Resources.UnloadAsset(prefab);
+        //         }
+        //     }
+        // }
     }
 }
