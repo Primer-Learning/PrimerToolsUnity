ebf9021a3482bb68a01aefe58f20b49ebe14829e
Matías Quezada <matias@primerlearning.org>
2023-06-27 13:22:38 +0200

    Review PoissonDisc

--- PrimerObject/PoissonDiscOverflowMode.cs
+++
@@ -1,8 +0,0 @@
-public enum PoissonDiscOverflowMode {
-    None,
-    // Squeeze mode doesn't seem to properly respect the new min distance
-    // Current guess for why is that the new grid boxes not only divide but 
-    // also shift when the dimensions are odd.
-    Squeeze,
-    Force
-}
