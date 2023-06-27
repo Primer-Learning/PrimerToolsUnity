4d295c89faef643b02759dfd71fa16a329505748
Matías Quezada <matias@primerlearning.org>
2023-06-27 22:06:19 +0200

    Clean up RandomElevationTerrain

--- Primer.Simulation/Terrain/RandomElevationTerrain.cs
+++ Primer.Simulation/Terrain/RandomElevationTerrain.cs
@@ -1,97 +1,92 @@
-﻿using Sirenix.Utilities.Editor;
-using UnityEditor;
+﻿using Sirenix.OdinInspector;
+using Sirenix.Utilities.Editor;
 using UnityEngine;
 
 namespace Primer.Simulation
 {
-    [ExecuteInEditMode]
+    [ExecuteAlways]
     [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
     public class RandomElevationTerrain : MonoBehaviour
     {
         /// <summary>A height that is taller higher than "any" terrain can go.</summary>
         public const float HIGHER_THAN_POSSIBLE = 10000f;
 
+        [OnValueChanged(nameof(Generate))]
+        [Tooltip("The natural size of cuboid. Will be used to determine the number of vertices along each dimension as well.")]
+        public Vector3Int size = new(50, 4, 50);
+
+        #region Internal fields
+        private MeshCollider meshColliderCache;
+        private MeshCollider meshCollider => transform.GetOrAddComponent(ref meshColliderCache);
+
+        private MeshFilter meshFilterCache;
+        private MeshFilter meshFilter => transform.GetOrAddComponent(ref meshFilterCache);
+        #endregion
+
+        #region Noise settings
+        [OnValueChanged(nameof(Generate))]
         public float noiseScale = 30;
-
+        [OnValueChanged(nameof(Generate))]
         public int octaves = 5;
+        [OnValueChanged(nameof(Generate))]
         [Range(0, 1)] public float persistance = 0.5f;
+        [OnValueChanged(nameof(Generate))]
         public float lacunarity = 1;
-
+        [OnValueChanged(nameof(Generate))]
         public int seed;
 
+        [OnValueChanged(nameof(Generate))]
         [Tooltip("Moves the region we sample elevation values from.")]
         public Vector2 offset = new(51.11f, 0);
+        #endregion
 
+        #region Mesh settings
+        [OnValueChanged(nameof(GenerateMesh))]
         [Tooltip("A constant that's multiplied with the unmultiplied elevation value (which will be bounded from 0 to 1).")]
         public float meshHeightMultiplier = 4;
 
-        [Tooltip("If true, the mesh will be regenerated in the editor when a field is changed.")]
-        public bool autoUpdate = true;
-
+        [OnValueChanged(nameof(GenerateMesh))]
         [Tooltip("Determines the roundness of the edges. Max roundness will be better if `size.y` is even.")]
         [Range(0, 1)]
         public float roundness = 1;
 
+        [OnValueChanged(nameof(GenerateMesh))]
         [Tooltip("A constant that's subtracted from the elevation value.")]
         public float elevationOffset = 2;
+        #endregion
 
-        [Tooltip("The natural size of cuboid. Will be used to determine the number of vertices along each dimension as well.")]
-        public Vector3Int size = new(50, 4, 50);
+        #region Mesh generation
+        private float[,] noiseMap;
 
-        [HideInInspector]
-        public Texture2D dirtPathMask;
-
-        public void PrepareTexture()
+        public void Generate()
         {
-            const TextureFormat desiredFormat = TextureFormat.R8;
-            const int resolutionMultiplier = 1;
+            noiseMap = Noise.GenerateNoiseMap(
+                size.To2D(),
+                seed,
+                noiseScale,
+                octaves,
+                persistance,
+                lacunarity,
+                offset
+            );
 
-            if (dirtPathMask && dirtPathMask.format == desiredFormat) {
-                if (dirtPathMask.width != size.x * resolutionMultiplier && dirtPathMask.height != size.z * resolutionMultiplier) {
-                    dirtPathMask = dirtPathMask.ResizeByBlit(size.x * resolutionMultiplier,
-                    size.z * resolutionMultiplier);
-                    Debug.Log("Resizing");
-                }
-            }
-            else {
-                dirtPathMask = new Texture2D(size.x * resolutionMultiplier, size.z * resolutionMultiplier, desiredFormat, false);
-                GetComponent<Renderer>().material.SetTexture("_PathMaskTex", dirtPathMask);
-                Debug.Log("New one!");
-            }
+            GenerateMesh();
         }
 
-#if UNITY_EDITOR
-        private SerializedObject lastValue;
-
-        private static bool CompareSerializedObjects(SerializedObject a, SerializedObject b)
+        private void GenerateMesh()
         {
-            // This will recursively check every property, even though it seems like it ought to
-            // only check the first. I found this out through experiment.
-            return SerializedProperty.DataEquals(a.GetIterator(), b.GetIterator());
+            var mesh = MeshGenerator.CreateMesh(
+                Mathf.FloorToInt(roundness * size.y / 2),
+                size,
+                noiseMap,
+                meshHeightMultiplier,
+                elevationOffset
+            );
+
+            meshCollider.sharedMesh = mesh;
+            meshFilter.sharedMesh = mesh;
         }
-
-        public void Update()
-        {
-            if (Application.isPlaying) return;
-            var current = new SerializedObject(this);
-
-            if (autoUpdate && lastValue is not null && !CompareSerializedObjects(current, lastValue)) {
-                GenerateMap();
-            }
-
-            lastValue = current;
-        }
-        #endif
-
-        public void GenerateMap()
-        {
-            var noiseMap = Noise.GenerateNoiseMap(size.x, size.z, seed, noiseScale, octaves,
-                persistance, lacunarity, offset);
-
-            GetComponent<MeshCollider>().sharedMesh = GetComponent<MeshFilter>().sharedMesh =
-                MeshGenerator.CreateMesh(Mathf.FloorToInt(roundness * size.y / 2), size, noiseMap,
-                    meshHeightMultiplier, elevationOffset);
-        }
+        #endregion
 
         /// <summary>Gets the position of the ground at the given (x, z)</summary>
         /// <param name="x">Local-to-terrain x</param>
@@ -105,9 +100,33 @@ namespace Primer.Simulation
             var down = transform.TransformDirection(Vector3.down);
             var ray = new Ray(pointAbove, down);
 
-            return GetComponent<MeshCollider>().Raycast(ray, out var hitInfo, float.PositiveInfinity)
+            return meshCollider.Raycast(ray, out var hitInfo, float.PositiveInfinity)
                 ? hitInfo.point
                 : transform.TransformPoint(new Vector3(x, 0, z));
         }
+
+#if UNITY_EDITOR
+        [HideInInspector]
+        public Texture2D dirtPathMask;
+
+        public void PrepareTexture()
+        {
+            const TextureFormat desiredFormat = TextureFormat.R8;
+            const int resolutionMultiplier = 1;
+
+            if (dirtPathMask && dirtPathMask.format == desiredFormat) {
+                if (dirtPathMask.width == size.x * resolutionMultiplier || dirtPathMask.height == size.z * resolutionMultiplier)
+                    return;
+
+                dirtPathMask = dirtPathMask.ResizeByBlit(size.x * resolutionMultiplier, size.z * resolutionMultiplier);
+                Debug.Log("Resizing");
+            }
+            else {
+                dirtPathMask = new Texture2D(size.x * resolutionMultiplier, size.z * resolutionMultiplier, desiredFormat, false);
+                GetComponent<Renderer>().material.SetTexture("_PathMaskTex", dirtPathMask);
+                Debug.Log("New one!");
+            }
+        }
+#endif
     }
 }
