6b1b845e2125a961d83288ca8d7b4d6f2fba0af3
Matías Quezada <matias@primerlearning.org>
2023-06-29 10:47:13 +0200

    Improve Hawk / Dove logic to make it more readable

--- Simulation.Evolution/Rules/ConflictResolutionRule/HawkDoveConflictResolution.cs
+++ Simulation.Evolution/Rules/ConflictResolutionRule/HawkDoveConflictResolution.cs
@@ -9,7 +9,12 @@ namespace Simulation.Evolution
 {
     public class HawkDoveConflictResolution : ConflictResolutionRule
     {
-        public class Hawk : MonoBehaviour {}
+        // These classes can be moved outside if they are used in more than one place
+        // to /Concepts/ folder maybe
+        // as long as it's only used inside this class, let's keep it here
+        public abstract class ConflictBehaviour : MonoBehaviour {}
+        public class Hawk : ConflictBehaviour {}
+        public class Dove : ConflictBehaviour {}
 
         public float hawkDoveRatio = 0.5f;
         public float hawkDoveBenefit = 0.5f;
@@ -19,37 +24,41 @@ namespace Simulation.Evolution
         {
             if (Random.value < hawkDoveRatio)
                 agent.GetOrAddComponent<Hawk>();
+            else
+                agent.GetOrAddComponent<Dove>();
         }
 
         public override async UniTask Resolve(IEnumerable<Agent> agents, Food food)
         {
             var (first, second) = agents.Shuffle().Take(2).ToList();
-            var firstIsHawk = first.GetComponent<Hawk>() is not null;
-            var secondIsHawk = second.GetComponent<Hawk>() is not null;
+            var firstBehaviour = first.GetComponent<ConflictBehaviour>();
+            var secondBehaviour = second.GetComponent<ConflictBehaviour>();
 
             await UniTask.WhenAll(
                 first.Eat(food),
                 second.Eat(food)
             );
 
-            switch (firstIsHawk, secondIsHawk) {
-                case (true, true):
-                    // they eat but waste the energy fighting
+            switch (firstBehaviour, secondBehaviour) {
+                case (Hawk, Hawk):
+                    // energy wasted on fighting
                     first.energy -= hawkHawkCost;
                     second.energy -= hawkHawkCost;
                     return;
 
-                case (true, false):
+                case (Hawk, Dove):
+                    // first steals from second
                     first.energy += hawkDoveBenefit;
                     second.energy -= hawkDoveBenefit;
                     return;
 
-                case (false, true):
+                case (Dove, Hawk):
+                    // second steals from first
                     first.energy -= hawkDoveBenefit;
                     second.energy += hawkDoveBenefit;
                     return;
 
-                case (false, false):
+                case (Dove, Dove):
                     return;
             }
         }
