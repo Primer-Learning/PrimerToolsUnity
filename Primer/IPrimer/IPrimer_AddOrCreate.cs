216370c01ecc1dcac93b70704a04b8ff2bceba94
Matías Quezada <matias@primerlearning.org>
2023-07-06 16:28:32 +0200

    primer.FindOrCreate() with cache

--- Primer/IPrimer/IPrimer_AddOrCreate.cs
+++ Primer/IPrimer/IPrimer_AddOrCreate.cs
@@ -10,10 +10,29 @@ namespace Primer
             return self.ToContainer().Add(name, options with { ignoreSiblingOrder = true });
         }
 
-        public static T FindOrCreate<T>(this IPrimer self, string name, ChildOptions options = null) where T : Component
+        public static Transform FindOrCreate(this IPrimer self, ref Transform cache, string name,
+            ChildOptions options = null)
+        {
+            if (cache == null)
+                cache = self.FindOrCreate(name, options);
+
+            return cache;
+        }
+
+        public static T FindOrCreate<T>(this IPrimer self, string name, ChildOptions options = null)
+            where T : Component
         {
             options ??= new ChildOptions();
             return self.ToContainer().Add<T>(name, options with { ignoreSiblingOrder = true });
+        }
+
+        public static T FindOrCreate<T>(this IPrimer self, ref T cache, string name, ChildOptions options = null)
+            where T : Component
+        {
+            if (cache == null)
+                cache = self.FindOrCreate<T>(name, options);
+
+            return cache;
         }
 
         public static Transform FindOrCreate(this Component self, string name, ChildOptions options = null)
@@ -22,11 +41,29 @@ namespace Primer
             return self.ToContainer().Add(name, options with { ignoreSiblingOrder = true });
         }
 
+        public static Transform FindOrCreate(this Component self, ref Transform cache, string name,
+            ChildOptions options = null)
+        {
+            if (cache == null)
+                cache = self.FindOrCreate(name, options);
+
+            return cache;
+        }
+
         public static T FindOrCreate<T>(this Component self, string name, ChildOptions options = null)
             where T : Component
         {
             options ??= new ChildOptions();
             return self.ToContainer().Add<T>(name, options with { ignoreSiblingOrder = true });
+        }
+
+        public static T FindOrCreate<T>(this Component self, ref T cache, string name, ChildOptions options = null)
+            where T : Component
+        {
+            if (cache == null)
+                cache = self.FindOrCreate<T>(name, options);
+
+            return cache;
         }
     }
 }
