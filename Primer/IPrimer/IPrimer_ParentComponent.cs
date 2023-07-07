16732d3e881416cb677d15cbbedfb943ee5bb13c
Matías Quezada <matias@primerlearning.org>
2023-07-07 11:34:44 +0200

    In order to support interfaces, for primer.ParentComponent<T>() T doesn't need to be a Component

--- Primer/IPrimer/IPrimer_ParentComponent.cs
+++ Primer/IPrimer/IPrimer_ParentComponent.cs
@@ -4,17 +4,17 @@ namespace Primer
 {
     public static class IPrimer_ParentComponent
     {
-        public static T ParentComponent<T>(this IPrimer self) where T : Component
+        public static T ParentComponent<T>(this IPrimer self)
         {
             return self.component.GetComponentInParent<T>();
         }
 
-        public static T[] ParentComponents<T>(this IPrimer self) where T : Component
+        public static T[] ParentComponents<T>(this IPrimer self)
         {
             return self.component.GetComponentsInParent<T>();
         }
 
-        public static T ParentComponent<T>(this IPrimer self, ref T cache) where T : Component
+        public static T ParentComponent<T>(this IPrimer self, ref T cache)
         {
             if (cache == null)
                 cache = self.component.GetComponentInParent<T>();
@@ -22,7 +22,7 @@ namespace Primer
             return cache;
         }
 
-        public static T[] ParentComponents<T>(this IPrimer self, ref T[] cache) where T : Component
+        public static T[] ParentComponents<T>(this IPrimer self, ref T[] cache)
         {
             if (cache is null || cache.Length is 0)
                 cache = self.component.GetComponentsInParent<T>();
@@ -30,17 +30,17 @@ namespace Primer
             return cache;
         }
 
-        public static T ParentComponent<T>(this Component self) where T : Component
+        public static T ParentComponent<T>(this Component self)
         {
             return self.GetComponentInParent<T>();
         }
 
-        public static T[] ParentComponents<T>(this Component self) where T : Component
+        public static T[] ParentComponents<T>(this Component self)
         {
             return self.GetComponentsInParent<T>();
         }
 
-        public static T ParentComponent<T>(this Component self, ref T cache) where T : Component
+        public static T ParentComponent<T>(this Component self, ref T cache)
         {
             if (cache == null)
                 cache = self.GetComponentInParent<T>();
@@ -48,7 +48,7 @@ namespace Primer
             return cache;
         }
 
-        public static T[] ParentComponents<T>(this Component self, ref T[] cache) where T : Component
+        public static T[] ParentComponents<T>(this Component self, ref T[] cache)
         {
             if (cache is null || cache.Length is 0)
                 cache = self.GetComponentsInParent<T>();
