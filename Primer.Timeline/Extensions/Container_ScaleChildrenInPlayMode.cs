568a5f507728b1dcee441a633ea826d9cdcdf532
Matías Quezada <matias@primerlearning.org>
2023-07-07 12:36:45 +0200

    Add duration to container.ScaleChildrenInPlayMode(duration)

--- Primer.Timeline/Extensions/Container_ScaleChildrenInPlayMode.cs
+++ Primer.Timeline/Extensions/Container_ScaleChildrenInPlayMode.cs
@@ -6,18 +6,18 @@ namespace Primer.Timeline
 {
     public static class Container_ScaleChildrenInPlayMode
     {
-        public static T ScaleChildrenInPlayMode<T>(this T self) where T : Container
+        public static T ScaleChildrenInPlayMode<T>(this T self, float duration = 0.5f) where T : Container
         {
-            self.onCreate = x => x.ScaleUpFromZero().PlayAndForget();
-            self.onRemove = (x, defer) => x.ShrinkAndDispose(defer: defer).Forget();
+            self.onCreate = x => (x.ScaleUpFromZero() with { duration = duration }).PlayAndForget();
+            self.onRemove = (x, defer) => x.ShrinkAndDispose(duration, defer).Forget();
             return self;
         }
 
-        public static T ScaleGrandchildrenInPlayMode<T>(this T self) where T : Container
+        public static T ScaleGrandchildrenInPlayMode<T>(this T self, float duration = 0.5f) where T : Container
         {
             self.onCreate = child => {
                 child.GetChildren()
-                    .Select(x => x.ScaleUpFromZero() with { duration = 0.5f })
+                    .Select(x => x.ScaleUpFromZero() with { duration = duration })
                     .RunInParallel()
                     .PlayAndForget();
             };
@@ -25,7 +25,7 @@ namespace Primer.Timeline
             self.onRemove = async (tick, defer) => {
                 var childrenRemoval = tick
                     .GetChildren()
-                    .Select(x => x.ShrinkAndDispose(0.1f));
+                    .Select(x => x.ShrinkAndDispose(duration));
 
                 await UniTask.WhenAll(childrenRemoval);
                 tick.Dispose();
