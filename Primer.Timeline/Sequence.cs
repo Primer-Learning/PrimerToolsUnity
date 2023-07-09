a392ad3011720c4346e4734f2dcbeedd725502e7
Matías Quezada <matias@primerlearning.org>
2023-07-09 19:46:50 +0200

    sequence.DisposeOnCleanup(component)

--- Primer.Timeline/Sequence.cs
+++ Primer.Timeline/Sequence.cs
@@ -8,11 +8,25 @@ namespace Primer.Timeline
     [DisallowMultipleComponent]
     public abstract class Sequence : AsyncMonoBehaviour
     {
+        private readonly List<Transform> disposeOnCleanup = new();
+
         // TODO: Remove
         [Obsolete("Code that used to be in Prepare() can be added at the beginning of Define(), before the first yield return, instead.")]
         public virtual void Prepare() {}
 
-        public virtual void Cleanup() {}
+        public virtual void Cleanup()
+        {
+            foreach (var child in disposeOnCleanup)
+                if (child != null)
+                    child.Dispose();
+
+            disposeOnCleanup.Clear();
+        }
+
+        protected void DisposeOnCleanup(Component child)
+        {
+            disposeOnCleanup.Add(child.transform);
+        }
 
         public SequenceRunner Run() => new(this);
 
