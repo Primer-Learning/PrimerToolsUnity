8664a86e77a226dda9cad1b85afb96d4c47c32cf
Justin Helps <justin.r.helps@gmail.com>
2023-07-13 11:27:00 -0500

    Make CalculateDifference its wown method so it can be used to make a vector field visualization.

--- Primer.Simulation/MathSim/GameTheorySim.cs
+++ Primer.Simulation/MathSim/GameTheorySim.cs
@@ -39,24 +39,38 @@ namespace Primer.Simulation
 
         private AlleleFrequency<T> SingleIteration(AlleleFrequency<T> previous)
         {
-            var list = previous
-                .Select(x => (
-                    strategy: x.Key,
-                    frequency: x.Value,
-                    fitness: CalculateFitness(x.Key, previous)
-                ))
-                .ToList();
-
-            var avgFitness = list.Average(x => x.fitness);
+            var difference = CalculateDifference(previous);
             var result = new AlleleFrequency<T>();
-
-            foreach (var (strategy, frequency, fitness) in list) {
-                var difference = frequency * (fitness - avgFitness) / avgFitness;
-                result[strategy] = frequency + stepSize * difference;
+            
+            foreach (var (strategy, _) in previous)
+            {
+                result[strategy] = previous[strategy] + stepSize * difference[strategy];
             }
 
             result.Normalize();
             return result;
+        }
+
+        // Pulled this out to make it easier to make a vector field visualization
+        public AlleleFrequency<T> CalculateDifference(AlleleFrequency<T> currentState)
+        {
+            var list = currentState
+                .Select(x => (
+                    strategy: x.Key,
+                    frequency: x.Value,
+                    fitness: CalculateFitness(x.Key, currentState)
+                ))
+                .ToList();
+            
+            var avgFitness = list.Average(x => x.fitness);
+
+            var difference = new AlleleFrequency<T>();
+            
+            foreach (var (strategy, frequency, fitness) in list) {
+                difference[strategy] = frequency * (fitness - avgFitness) / avgFitness;
+            }
+
+            return difference;
         }
 
         private float CalculateFitness(T strategy, AlleleFrequency<T> previous)
