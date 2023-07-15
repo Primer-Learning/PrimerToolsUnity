97c0ef8e22b017c16592efc137b174050ba99eb1
Matías Quezada <matias@primerlearning.org>
2023-07-15 13:59:42 +0200

    Use TableMatrix for strategy match-up rewards

--- Primer.Simulation/MathSim/RewardEditorComponent.cs
+++ Primer.Simulation/MathSim/RewardEditorComponent.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 using Primer.Simulation;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public abstract class RewardEditorComponent<T> : MonoBehaviour where T : Enum
 {
     // Obviously there's a more general way to handle triggering/listening, but this is straightforward for now.
+    [PropertyOrder(-2)]
     public TernaryVectorFieldPlotter vectorField;
+
     private void OnValidate()
     {
         if (vectorField is not null)
             vectorField.TriggeredPlotArrows();
     }
     
-    public RewardMatrix<T> rewardMatrix {
-        get
-        {
-            return new RewardMatrix<T>(PutValuesInMatrix());
-        }
-    }
+    public RewardMatrix<T> rewardMatrix => new(PutValuesInMatrix());
 
     public abstract float[,] PutValuesInMatrix();
 }
