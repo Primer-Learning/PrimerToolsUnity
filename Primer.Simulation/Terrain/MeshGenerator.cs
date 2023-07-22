490c734e6950bdf8c152660ad8474d66ab8f61e2
Justin Helps <justin.r.helps@gmail.com>
2023-07-21 20:13:29 -0500

    Implement a system of remapping vertices that are actually passed to the mesh and trianges to avoid making duplicates, which messes up normals. Still work needed, but progress.

--- Primer.Simulation/Terrain/MeshGenerator.cs
+++ Primer.Simulation/Terrain/MeshGenerator.cs
@@ -29,6 +29,7 @@ namespace Primer.Simulation
             };
 
             generator.CreateVertices();
+            generator.AssignVertices();
             generator.CreateTriangles();
 
             if (cleanUp)
@@ -36,7 +37,7 @@ namespace Primer.Simulation
                 generator.CleanDuplicateVerticesAndZeroAreaTriangles();
             }
 
-            // generator.mesh.RecalculateNormals();
+            generator.mesh.RecalculateNormals();
             generator.mesh.RecalculateTangents();
 
             return generator.mesh;
@@ -52,8 +53,9 @@ namespace Primer.Simulation
         private Vector3[] normals;
         private float roundingRadius;
         private float edgeClampFactor;
-        private Vector2[] uv;
         private Vector3[] vertices;
+        private int[] mapFromUnusedToUsedSystematicIndices;
+        private List<int> mapFromTempToRealVertices;
         private int xSize, ySize, zSize;
 
         private MeshGenerator() { }
@@ -66,7 +68,7 @@ namespace Primer.Simulation
                                 (ySize - 1) * (zSize - 1)) * 2;
 
             vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
-            uv = new Vector2[vertices.Length];
+            mapFromUnusedToUsedSystematicIndices = new int[vertices.Length];
             normals = new Vector3[vertices.Length];
 
             var v = 0;
@@ -85,10 +87,25 @@ namespace Primer.Simulation
             for (var z = 1; z < zSize; z++)
             for (var x = 1; x < xSize; x++)
                 SetVertex(v++, x, 0, z);
-
-            mesh.vertices = vertices;
-            mesh.normals = normals;
-            mesh.uv = uv;
+        }
+        
+        private void AssignVertices()
+        {
+            // Create a list of vertices where mapFromUnusedToUsedSystematicIndices[i] = i
+            mapFromTempToRealVertices = new List<int>();
+            var realVertices = new List<Vector3>();
+            var realNormals = new List<Vector3>();
+            for (var i = 0; i < mapFromUnusedToUsedSystematicIndices.Length; i++)
+            {
+                if (mapFromUnusedToUsedSystematicIndices[i] == i)
+                {
+                    realVertices.Add(vertices[i]);
+                    realNormals.Add(normals[i]);
+                    mapFromTempToRealVertices.Add(i);
+                }
+            }
+            mesh.vertices = realVertices.ToArray();
+            mesh.normals = realNormals.ToArray();
         }
 
         private void SetVertex(int i, int x, int y, int z)
@@ -96,11 +113,36 @@ namespace Primer.Simulation
             vertices[i] = new Vector3(x, y, z);
             
             var innerDifference = CalculateDistanceFromInnerSurface(x, y, z, roundingRadius);
-
+            
             var verticalFace = innerDifference.magnitude > roundingRadius;
             if (verticalFace) {
                 normals[i] = innerDifference.normalized;
                 vertices[i] = new Vector3(x, y, z) - innerDifference + innerDifference.normalized * roundingRadius;
+                
+                if (x == 0 || x == xSize || z == 0 || z == zSize) {
+                    // This is an edge vertex, so it should map to itself
+                    mapFromUnusedToUsedSystematicIndices[i] = i;
+                }
+                else if (x < xSize / 2)
+                {
+                    // Later, this will essentially tell the mesh to use the vertex to the right of this one
+                    mapFromUnusedToUsedSystematicIndices[i] = i + 1;
+                }
+                else if (x > xSize / 2)
+                {
+                    // Later, this will essentially tell the mesh to use the vertex to the left of this one
+                    mapFromUnusedToUsedSystematicIndices[i] = i - 1;
+                }
+                else
+                {
+                    // This is the middle vertex, so it should map to itself
+                    mapFromUnusedToUsedSystematicIndices[i] = i;
+                }
+                
+            }
+            else
+            {
+                mapFromUnusedToUsedSystematicIndices[i] = i;
             }
             
             // Make bottom face normals point down
@@ -114,8 +156,6 @@ namespace Primer.Simulation
             {
                 normals[i] = (Vector3.up + normals[i]).normalized;
             }
-            
-            uv[i] = new Vector2(x / (float)xSize, z / (float)zSize);
 
             // If this point is in the bottom half, we're done.
             if (y < (float) ySize / 2)
@@ -130,6 +170,17 @@ namespace Primer.Simulation
             if (edgeClampFactor > 0) elevationAdjustment *= EdgeElevationDamping(vertices[i]);
 
             vertices[i].y += elevationAdjustment + elevationOffset;
+        }
+        
+        private int GetUsedSystematicIndexFromSystematicIndex(int i)
+        {
+            if (mapFromUnusedToUsedSystematicIndices[i] == i) return i;
+            return GetUsedSystematicIndexFromSystematicIndex(mapFromUnusedToUsedSystematicIndices[i]);
+        }
+        private int GetIndexOfRealVertexFromFakeIndex(int i)
+        {
+            // Debug.Log($"FakeVertexCount: {mapFromUnusedToUsedSystematicIndices.Length}, TempVertexCount: {mapFromTempToRealVertices.Count}, RealVertexCount: {mesh.vertices.Length}");
+            return mapFromTempToRealVertices.IndexOf(GetUsedSystematicIndexFromSystematicIndex(i));
         }
         
         private float EdgeElevationDamping(Vector3 vertex)
@@ -251,92 +302,99 @@ namespace Primer.Simulation
 
         private void CreateTriangles()
         {
-            var trianglesZ = new int[xSize * ySize * 12];
-            var trianglesX = new int[ySize * zSize * 12];
-            var trianglesY = new int[xSize * zSize * 12];
+            var trianglesZ = new List<int>();
+            var trianglesX =  new List<int>();
+            var trianglesY = new List<int>();
             var ring = (xSize + zSize) * 2;
-            int tZ = 0, tX = 0, tY = 0, v = 0;
+            var v = 0;
 
             for (var y = 0; y < ySize; y++, v++) {
                 for (var q = 0; q < xSize; q++, v++)
-                    tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
+                    SetQuad(trianglesZ, v, v + 1, v + ring, v + ring + 1);
                 for (var q = 0; q < zSize; q++, v++)
-                    tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
+                    SetQuad(trianglesX, v, v + 1, v + ring, v + ring + 1);
                 for (var q = 0; q < xSize; q++, v++)
-                    tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
+                    SetQuad(trianglesZ, v, v + 1, v + ring, v + ring + 1);
                 for (var q = 0; q < zSize - 1; q++, v++)
-                    tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
-                tX = SetQuad(trianglesX, tX, v, v - ring + 1, v + ring, v + 1);
+                    SetQuad(trianglesX, v, v + 1, v + ring, v + ring + 1);
+                SetQuad(trianglesX, v, v - ring + 1, v + ring, v + 1);
             }
 
-            tY = CreateTopFace(trianglesY, tY, ring);
-            tY = CreateBottomFace(trianglesY, tY, ring);
+            CreateTopFace(trianglesY, trianglesZ.Count + trianglesX.Count, ring);
+            CreateBottomFace(trianglesY, trianglesZ.Count + trianglesX.Count + trianglesY.Count, ring);
             mesh.SetTriangles(trianglesZ.Concat(trianglesX).Concat(trianglesY).ToArray(), 0);
         }
 
-        private int CreateTopFace(int[] triangles, int t, int ring)
+        private int CreateTopFace(List<int> triangles, int t, int ring)
         {
             var v = ring * ySize;
             for (var x = 0; x < xSize - 1; x++, v++)
-                t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + ring);
-            t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + 2);
+                SetQuad(triangles, v, v + 1, v + ring - 1, v + ring);
+            SetQuad(triangles, v, v + 1, v + ring - 1, v + 2);
 
             var vMin = ring * (ySize + 1) - 1;
             var vMid = vMin + 1;
             var vMax = v + 2;
 
             for (var z = 1; z < zSize - 1; z++, vMin--, vMid++, vMax++) {
-                t = SetQuad(triangles, t, vMin, vMid, vMin - 1, vMid + xSize - 1);
+                SetQuad(triangles,  vMin, vMid, vMin - 1, vMid + xSize - 1);
                 for (var x = 1; x < xSize - 1; x++, vMid++)
-                    t = SetQuad(triangles, t, vMid, vMid + 1, vMid + xSize - 1, vMid + xSize);
-                t = SetQuad(triangles, t, vMid, vMax, vMid + xSize - 1, vMax + 1);
+                    SetQuad(triangles, vMid, vMid + 1, vMid + xSize - 1, vMid + xSize);
+                SetQuad(triangles, vMid, vMax, vMid + xSize - 1, vMax + 1);
             }
 
             var vTop = vMin - 2;
-            t = SetQuad(triangles, t, vMin, vMid, vTop + 1, vTop);
+            SetQuad(triangles, vMin, vMid, vTop + 1, vTop);
             for (var x = 1; x < xSize - 1; x++, vTop--, vMid++)
-                t = SetQuad(triangles, t, vMid, vMid + 1, vTop, vTop - 1);
-            t = SetQuad(triangles, t, vMid, vTop - 2, vTop, vTop - 1);
+                SetQuad(triangles, vMid, vMid + 1, vTop, vTop - 1);
+            SetQuad(triangles, vMid, vTop - 2, vTop, vTop - 1);
 
             return t;
         }
 
-        private int CreateBottomFace(int[] triangles, int t, int ring)
+        private int CreateBottomFace(List<int> triangles, int t, int ring)
         {
             var v = 1;
             var vMid = vertices.Length - (xSize - 1) * (zSize - 1);
-            t = SetQuad(triangles, t, ring - 1, vMid, 0, 1);
+            SetQuad(triangles, ring - 1, vMid, 0, 1);
             for (var x = 1; x < xSize - 1; x++, v++, vMid++)
-                t = SetQuad(triangles, t, vMid, vMid + 1, v, v + 1);
-            t = SetQuad(triangles, t, vMid, v + 2, v, v + 1);
+                SetQuad(triangles, vMid, vMid + 1, v, v + 1);
+            SetQuad(triangles, vMid, v + 2, v, v + 1);
 
             var vMin = ring - 2;
             vMid -= xSize - 2;
             var vMax = v + 2;
 
             for (var z = 1; z < zSize - 1; z++, vMin--, vMid++, vMax++) {
-                t = SetQuad(triangles, t, vMin, vMid + xSize - 1, vMin + 1, vMid);
+                SetQuad(triangles, vMin, vMid + xSize - 1, vMin + 1, vMid);
                 for (var x = 1; x < xSize - 1; x++, vMid++)
-                    t = SetQuad(triangles, t, vMid + xSize - 1, vMid + xSize, vMid, vMid + 1);
-                t = SetQuad(triangles, t, vMid + xSize - 1, vMax + 1, vMid, vMax);
+                    SetQuad(triangles, vMid + xSize - 1, vMid + xSize, vMid, vMid + 1);
+                SetQuad(triangles, vMid + xSize - 1, vMax + 1, vMid, vMax);
             }
 
             var vTop = vMin - 1;
-            t = SetQuad(triangles, t, vTop + 1, vTop, vTop + 2, vMid);
+            SetQuad(triangles, vTop + 1, vTop, vTop + 2, vMid);
             for (var x = 1; x < xSize - 1; x++, vTop--, vMid++)
-                t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
-            t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vTop - 2);
+                SetQuad(triangles, vTop, vTop - 1, vMid, vMid + 1);
+            SetQuad(triangles, vTop, vTop - 1, vMid, vTop - 2);
 
             return t;
         }
 
-        private static int SetQuad(int[] triangles, int i, int v00, int v10, int v01, int v11)
+        private void SetQuad(List<int> triangles, int v00, int v10, int v01, int v11)
         {
-            triangles[i] = v00;
-            triangles[i + 1] = triangles[i + 4] = v01;
-            triangles[i + 2] = triangles[i + 3] = v10;
-            triangles[i + 5] = v11;
-            return i + 6;
+            SetTriangle(triangles, v00, v01, v10);
+            SetTriangle(triangles, v01, v11, v10);
+        }
+
+        private void SetTriangle(List<int> triangles, int v0, int v1, int v2)
+        {
+            if (AreAllUnique(v0, v1, v2))
+            {
+                triangles.Add(GetIndexOfRealVertexFromFakeIndex(v0));
+                triangles.Add(GetIndexOfRealVertexFromFakeIndex(v1));
+                triangles.Add(GetIndexOfRealVertexFromFakeIndex(v2));
+            }
         }
 
         private bool AreAllUnique(params int[] indices)
