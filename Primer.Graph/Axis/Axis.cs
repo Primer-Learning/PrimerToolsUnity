6bb5d93e156737ed36a67c9c8a3a0a06364c5758
Matías Quezada <matias@primerlearning.org>
2023-07-31 19:04:26 +0200

    axis.GrowFromOrigin()

--- Primer.Graph/Axis/Axis.cs
+++ Primer.Graph/Axis/Axis.cs
@@ -1,4 +1,5 @@
 using System;
+using Primer.Animation;
 using Primer.Timeline;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -18,7 +19,9 @@ namespace Primer.Graph
 
         public float DomainToPosition(float domainValue)
         {
-            return domainValue * scale;
+            return isActiveAndEnabled
+                ? domainValue * scale
+                : 0;
         }
 
         internal bool ListenDomainChange(Action listener)
@@ -33,6 +36,37 @@ namespace Primer.Graph
 
         public void OnEnable() => UpdateChildren();
         public void OnValidate() => UpdateChildren();
+
+
+        public Tween GrowFromOrigin(float newMax) => GrowFromOrigin(min, newMax);
+        public Tween GrowFromOrigin(float newMin, float newMax)
+        {
+            min = 0;
+            max = 0;
+
+            this.SetActive(true);
+
+            return Tween.Parallel(
+                delayBetweenStarts: 0.1f,
+                this.ScaleTo(1, initialScale: 0),
+                Tween.Parallel(
+                    Tween.Value(() => min, from: 0, to: newMin),
+                    Tween.Value(() => max, from: 0, to: newMax)
+                )
+            );
+        }
+
+        public Tween ShrinkToOrigin()
+        {
+            return Tween.Parallel(
+                delayBetweenStarts: 0.1f,
+                Tween.Parallel(
+                    Tween.Value(() => min, from: min, to: 0),
+                    Tween.Value(() => max, from: max, to: 0)
+                ),
+                this.ScaleTo(0, initialScale: 1)
+            );
+        }
 
 
         [Button(ButtonSizes.Large)]
