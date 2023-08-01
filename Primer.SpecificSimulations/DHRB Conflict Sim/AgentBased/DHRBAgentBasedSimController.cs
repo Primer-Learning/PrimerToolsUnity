33dda599ef11d95ec8b6175019367667fe63af13
Justin Helps <justin.r.helps@gmail.com>
2023-08-01 08:44:57 -0500

    Add ternary plot tracking to sim.

--- Primer.SpecificSimulations/DHRB Conflict Sim/AgentBased/DHRBAgentBasedSimController.cs
+++ Primer.SpecificSimulations/DHRB Conflict Sim/AgentBased/DHRBAgentBasedSimController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
+using Primer.Animation;
+using Primer.Graph;
 using Primer.Simulation;
 using Simulation.GameTheory;
 using UnityEngine;
@@ -11,6 +13,8 @@ namespace Primer.SpecificSimulations
     public class DHRBAgentBasedSimController : AgentBasedEvoGameTheorySimController<DHRB>
     {
         public Graph.Graph graph;
+        public TernaryPlot ternaryPlot;
+        
         protected override void SetStrategyRule()
         {
             strategyRule = new DHRBStrategyRule();
@@ -19,42 +23,79 @@ namespace Primer.SpecificSimulations
 
         protected override async UniTask OnSimStart()
         {
-            if (graph is null) return;
-            
             var numDoves = _sim.agents.Count(agent => agent.strategy.Equals(DHRB.Dove));
             var numHawks = _sim.agents.Count(agent => agent.strategy.Equals(DHRB.Hawk));
             var numRetaliators = _sim.agents.Count(agent => agent.strategy.Equals(DHRB.Retaliator));
             var numBullies = _sim.agents.Count(agent => agent.strategy.Equals(DHRB.Bully));
+            var total = numDoves + numHawks + numRetaliators + numBullies;
             
-            using var stackedArea = graph.AddStackedArea("Allele frequencies");
-            stackedArea.enabled = true;
-            stackedArea.colors =
-                new List<Color>() {PrimerColor.blue, PrimerColor.red, PrimerColor.green, PrimerColor.yellow};
-            stackedArea.SetData(
-                new float [] {numDoves},
-                new float [] {numHawks}, 
-                new float [] {numRetaliators},
-                new float [] {numBullies}
-            );
+            if (graph is not null)
+            {
+                
+                using var stackedArea = graph.AddStackedArea("Allele frequencies");
+                stackedArea.enabled = true;
+                stackedArea.colors =
+                    new List<Color>() {PrimerColor.blue, PrimerColor.red, PrimerColor.green, PrimerColor.yellow};
+                stackedArea.SetData(
+                    new float [] {numDoves},
+                    new float [] {numHawks}, 
+                    new float [] {numRetaliators},
+                    new float [] {numBullies}
+                );
+                
+                await stackedArea.GrowFromStart();
+            }
 
-            await stackedArea.GrowFromStart();
+            if (ternaryPlot is not null)
+            {
+                var gnome = ternaryPlot.GetContentGnome();
+
+                var blob = gnome.AddPrefab<PrimerBlob>("blob_skinned", "blob");
+                blob.transform.localScale = Vector3.zero;
+                blob.transform.localPosition = TernaryPlot.CoordinatesToPosition(
+                    (float) numDoves / total,
+                    (float) numHawks / total,
+                    (float) numRetaliators / total
+                );
+                await blob.transform.ScaleTo(Vector3.one / ternaryPlot.transform.localScale.x);
+            }
+            Debug.Log("Sim started");
         }
 
         protected override async UniTask OnCycleCompleted()
         {
-            if (graph is null) return;
-            
             var numDoves = _sim.agents.Count(agent => agent.strategy.Equals(DHRB.Dove));
             var numHawks = _sim.agents.Count(agent => agent.strategy.Equals(DHRB.Hawk));
             var numRetaliators = _sim.agents.Count(agent => agent.strategy.Equals(DHRB.Retaliator));
             var numBullies = _sim.agents.Count(agent => agent.strategy.Equals(DHRB.Bully));
-            
-            using var stackedArea = graph.AddStackedArea("Allele frequencies");
-            stackedArea.colors =
-                new List<Color>() {PrimerColor.blue, PrimerColor.red, PrimerColor.green, PrimerColor.yellow};
-            stackedArea.AddData(numDoves, numHawks, numRetaliators, numBullies);
+            var total = numDoves + numHawks + numRetaliators + numBullies;
 
-            await stackedArea.Transition();
+            if (graph is not null)
+            {
+                using var stackedArea = graph.AddStackedArea("Allele frequencies");
+                stackedArea.colors =
+                    new List<Color>() {PrimerColor.blue, PrimerColor.red, PrimerColor.green, PrimerColor.yellow};
+                stackedArea.AddData(numDoves, numHawks, numRetaliators, numBullies);
+
+                await stackedArea.Transition();
+            }
+            
+            if (ternaryPlot is not null)
+            {
+                var gnome = ternaryPlot.GetContentGnome();
+
+                var blob = gnome.AddPrefab<PrimerBlob>("blob_skinned", "blob");
+                blob.transform.localScale = Vector3.one / ternaryPlot.transform.localScale.x;
+                
+                await blob.transform.MoveTo(
+                    TernaryPlot.CoordinatesToPosition(
+                        (float) numDoves / total,
+                        (float) numHawks / total,
+                        (float) numRetaliators / total
+                    )
+                );
+            }
+            Debug.Log("Cycle completed");
         }
     }
 }
