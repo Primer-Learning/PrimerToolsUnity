1544ac8770d582813d9ef7712df70334363e5c0f
Justin Helps <justin.r.helps@gmail.com>
2023-08-30 14:42:12 -0500

    Make LandscapeItem look harder for a relevant landscape.

--- Primer.Simulation/Terrain/LandscapeItem.cs
+++ Primer.Simulation/Terrain/LandscapeItem.cs
@@ -10,9 +10,27 @@ namespace Primer.Simulation
 
         public Landscape landscape
         {
-            get => _landscape ? _landscape : transform.GetComponentInParent<Landscape>();
+            get => _landscape ? _landscape : FindLandscape();
             set => _landscape = value;
-        } 
+        }
+
+        private Landscape FindLandscape()
+        {
+            // Check a few possibilities for where the landscape might be
+            var landscapeCandidate = transform.GetComponentInParent<Landscape>();
+            if (landscapeCandidate != null) return landscapeCandidate;
+            
+            var landscapeCandidates = transform.parent.GetComponentsInChildren<Landscape>();
+            if (landscapeCandidates.Length > 1) Debug.LogWarning("FindLandscape: Multiple landscapes found, using first one");
+            if (landscapeCandidates.Length > 0) return landscapeCandidates[0];
+            
+            landscapeCandidates = FindObjectsOfType<Landscape>();
+            if (landscapeCandidates.Length > 1) Debug.LogWarning("FindLandscape: Multiple landscapes found, using first one");
+            if (landscapeCandidates.Length > 0) return landscapeCandidates[0];
+            
+            Debug.LogError("FindLandscape: No landscape found");
+            return null;
+        }
 
         [Button]
         public void TouchGround()
