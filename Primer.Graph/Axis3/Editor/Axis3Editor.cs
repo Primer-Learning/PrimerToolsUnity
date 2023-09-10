203cb89316ad90ee4f67cb8dbf588d7b87ba8a45
Justin Helps <justin.r.helps@gmail.com>
2023-09-10 16:04:08 -0500

    Make custom editor for Axis3 so we can do something akin to OnValidate without it being called on prefabs.

---
+++ Primer.Graph/Axis3/Editor/Axis3Editor.cs
@@ -0,0 +1,24 @@
+﻿using Primer.Graph;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Axis3))]
+public class Axis3Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Start change check
+        EditorGUI.BeginChangeCheck();
+        
+        // Draw the default inspector
+        DrawDefaultInspector();
+
+        // End change check
+        if (EditorGUI.EndChangeCheck())
+        {
+            // If here, some property was changed. Perform your actions.
+            var axis = (Axis3)target;
+            axis.UpdateChildren();
+        }
+    }
+}
