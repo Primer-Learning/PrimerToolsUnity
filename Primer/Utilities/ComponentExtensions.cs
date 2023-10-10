e33c1c354de781a3eccb1fff4fa9ddbbd6f4c193
Justin Helps <justin.r.helps@gmail.com>
2023-10-10 17:00:13 -0500

    Add a utility that allows components to copy public values from another.

---
+++ Primer/Utilities/ComponentExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace Primer
+{
+    public static class ComponentExtensions
+    {
+        public static void CopyPublicComponentValuesFrom<T>(this T destination, T source) where T : Component
+        {
+            // If either source or destination is null, do nothing
+            if (source == null || destination == null)
+            {
+                Debug.LogWarning("Source or destination is null. Cannot copy component values.");
+                return;
+            }
+
+            FieldInfo[] fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            foreach (FieldInfo field in fields)
+            {
+                field.SetValue(destination, field.GetValue(source));
+            }
+
+            PropertyInfo[] properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo property in properties)
+            {
+                // Skip properties that don't have a setter
+                if (property.CanWrite)
+                {
+                    property.SetValue(destination, property.GetValue(source, null), null);
+                }
+            }
+        }
+    }
+}
