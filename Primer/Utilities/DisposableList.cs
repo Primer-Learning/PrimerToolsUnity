0412cfc1920ba6865f38b166048ed95657609908
Justin Helps <justin.r.helps@gmail.com>
2023-10-17 10:20:47 -0500

    Add a DisposableList class, which is a list that implements IDisposable, allowing the 'using' pattern to be used on collections in scenes.

---
+++ Primer/Utilities/DisposableList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Primer
+{
+    public class DisposableList<T> : List<T>, IDisposable where T : IDisposable
+    {
+        public void Dispose()
+        {
+            foreach (var item in this)
+                item.Dispose();
+        }
+    }
+}
