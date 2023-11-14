7085c637f28c1a02ecdb02067074ae272844386a
Justin Helps <justin.r.helps@gmail.com>
2023-11-14 13:30:14 -0600

    Add sim recording and playback settings.

---
+++ Primer.Simulation/SimRecordingSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Primer.Simulation
+{
+    public enum SimRecordingSettings
+    {
+        None, // None!
+        Record, // Results will be saved to a JSON file
+        PlaybackDataWithVisuals, // Useful for ensuring the results in a rendered run
+        PlaybackDataWithoutVisuals // Useful for working on scenes that depend on data, but without all the sim objects
+    }
+}
