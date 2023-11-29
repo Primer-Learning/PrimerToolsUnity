e4f3644acca53a03bdf29eb61ece73d82025cd9b
Justin Helps <justin.r.helps@gmail.com>
2023-11-29 17:20:25 -0600

    Change SimRecording enum names

--- Primer.Simulation/SimRecordingSettings.cs
+++ Primer.Simulation/SimRecordingSettings.cs
@@ -4,7 +4,7 @@
     {
         None, // None!
         Record, // Results will be saved to a JSON file
-        PlaybackDataWithVisuals, // Useful for ensuring the results in a rendered run
-        PlaybackDataWithoutVisuals // Useful for working on scenes that depend on data, but without all the sim objects
+        VisualPlayback, // Useful for ensuring the results in a rendered run
+        DataPlayback // Useful for working on scenes that depend on data, but without all the sim objects
     }
 }
